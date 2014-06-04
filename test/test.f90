416d3817107d2a641a501b5f030d2451a692efaa
Zhiqi Huang <zhiqihuang@gmail.com>
2014-06-04 13:57:08 -0400

    SURP DE model in test
    SURP DE model in test

--- test/test.f90
+++ test/test.f90
@@ -15,20 +15,22 @@ program test
 
 !!example for coupled quintessence model 
 !!V(phi) = c0 / phi^n + c1
+!!I am not doing perfect initial conditions, you might get some initial small oscillations
 
   COOP_INT, parameter::nvars = 3  !!number of variables to evolve
   COOP_INT, parameter::nsteps = 1000  !!number of steps for output
   COOP_REAL, parameter:: MPl = 1.d0 !!define mass unit
   COOP_REAL, parameter:: H0 = 1.d0  !!define time unit
-
+  COOP_REAL:: Q, c_run
   COOP_INT, parameter::npower = 1
-  COOP_REAL, parameter:: Q = 1./coop_sqrt6
   COOP_REAL,parameter:: rhom_ini = 1.e4 * (Mpl**2*H0**2)
-  COOP_REAL,parameter:: phieq_ini = 0.01 * Mpl  
+  COOP_REAL,parameter:: phieq_ini = 0.005 * Mpl  
   COOP_REAL, parameter::c_flat = rhom_ini* 1.e-4
-  COOP_REAL, parameter::c_run = Q * rhom_ini * phieq_ini ** (npower + 1)/npower
   COOP_REAL, parameter::lna_start = 0.
   COOP_REAL, parameter::lna_end = log(rhom_ini/c_flat)/3.d0 + 3.d0  !!add  3 more efolds to make sure rho_m < rho_phi happens
+  COOP_INT:: itoday
+
+  COOP_INT:: iQ
 
   type(coop_ode)::co
   type(coop_arguments)::args
@@ -37,37 +39,64 @@ program test
   COOP_REAL:: phi(nsteps), lna(nsteps), dotphi(nsteps), w(nsteps), Ev, Ek, Vpp, deltaphi
   type(coop_asy)::fp
 
-  args = coop_arguments( i = (/ npower /),  r = (/ Q, c_run, c_flat /) )
-  hubble_ini = sqrt((potential(phieq_ini, args) + rhom_ini)/(3.d0*Mpl**2))
-  phieq_dot_ini = 3.d0*hubble_ini/(npower+1.d0)*phieq_ini
-  deltaphi = phieq_ini/100.d0
-  Vpp = (dVdphi(phieq_ini+deltaphi, args)- dVdphi(phieq_ini-deltaphi, args))/(2.d0*deltaphi)
-  deltaphi = -3.d0*hubble_ini*phieq_dot_ini/Vpp
-  if(abs(deltaphi/phieq_ini).gt. 0.1d0)then
-     write(*,*) "V''=", Vpp
-     write(*,*) deltaphi/Mpl, phieq_ini/Mpl
-     stop "deltaphi too big"
-  endif
-  call co%init(n = nvars, method = COOP_ODE_DVERK)  !!initialize the ode solver
-  call co%set_arguments(args = args)
-  call co%set_initial_conditions( xini = lna_start, yini = (/ phieq_ini+deltaphi, phieq_dot_ini, rhom_ini /) )
-  call coop_set_uniform(nsteps, lna, lna_start, lna_end)
-  do  i = 1, nsteps
-     if(i.gt.1)call co%evolve(get_yprime, lna(i))
-     phi(i) = co%PHI / Mpl
-     dotphi(i) = co%PHIDOT
-     Ek = dotphi(i)**2/2.d0
-     Ev = potential(phi(i), args)
-     w(i) = (Ek -Ev)/(Ek+Ev)
-  enddo
-  call fp%open("phi.txt")
-  call fp%init(xlabel = "$\ln a$", ylabel = "$\phi / M_p$")
-  call coop_asy_curve(fp, x=lna, y=phi, color = "black", linewidth = 1.5, linetype="solid")
-  call fp%close()
-
   call fp%open("w.txt")
-  call fp%init(xlabel = "$\ln a$", ylabel = "$w_\phi$")
-  call coop_asy_curve(fp, x=lna, y=w, color = "black", linewidth = 1.5, linetype="solid")
+  call fp%init(xlabel = "$\ln a$", ylabel = "$w_\phi$", caption = "$\phi_{\rm ini} = "//trim(coop_num2str(phieq_ini/Mpl))//"M_p$" )
+     
+
+  do iQ = 1, 3
+     Q = iQ * 0.15
+     c_run = Q * rhom_ini * phieq_ini ** (npower + 1)/npower
+     args = coop_arguments( i = (/ npower /),  r = (/ Q, c_run, c_flat /) )
+     hubble_ini = sqrt((potential(phieq_ini, args) + rhom_ini)/(3.d0*Mpl**2))
+     phieq_dot_ini = 3.d0*hubble_ini/(npower+1.d0)*phieq_ini
+     deltaphi = phieq_ini/100.d0
+     Vpp = (dVdphi(phieq_ini+deltaphi, args)- dVdphi(phieq_ini-deltaphi, args))/(2.d0*deltaphi)
+     deltaphi = -3.d0*hubble_ini*phieq_dot_ini/Vpp
+     if(abs(deltaphi/phieq_ini).gt. 0.1d0)then
+        write(*,*) "V''=", Vpp
+        write(*,*) deltaphi/Mpl, phieq_ini/Mpl
+        stop "deltaphi too big"
+     endif
+     call co%init(n = nvars, method = COOP_ODE_DVERK)  !!initialize the ode solver
+     call co%set_arguments(args = args)
+     call co%set_initial_conditions( xini = lna_start, yini = (/ phieq_ini+deltaphi, phieq_dot_ini, rhom_ini /) )
+     call coop_set_uniform(nsteps, lna, lna_start, lna_end)
+     itoday = 0
+     do  i = 1, nsteps
+        if(i.gt.1)call co%evolve(get_yprime, lna(i))
+        phi(i) = co%PHI / Mpl
+        dotphi(i) = co%PHIDOT
+        Ek = dotphi(i)**2/2.d0
+        Ev = potential(phi(i), args)
+        w(i) = (Ek -Ev)/(Ek+Ev)
+        if((Ek+Ev)/co%RHOM .ge. (0.7/0.3) .and. itoday.eq.0)then
+           itoday = i
+        endif
+     enddo
+     if(itoday .eq. 0)then
+        write(*,*) "you might want to increase lna_end"
+        stop
+     endif
+     lna = lna - lna(itoday)  !!renormalize a=1 today
+
+     select case(iQ)
+     case(1)
+        call coop_asy_curve(fp, x=lna(1:itoday), y=w(1:itoday), &
+             color = "black", linewidth = 1., linetype="solid", &
+             legend = "Q = "//trim(coop_num2str(co%Q_COUPLING, "(G10.4)")) )
+     case(2)
+        call coop_asy_curve(fp, x=lna(1:itoday), y=w(1:itoday), &
+             color = "red", linewidth = 1.5, linetype="dotted", &
+             legend = "Q = "//trim(coop_num2str(co%Q_COUPLING, "(G10.3)")) )
+     case(3)
+        call coop_asy_curve(fp, x=lna(1:itoday), y=w(1:itoday), &
+             color = "blue", linewidth = 1.5, linetype="dashed", &
+             legend = "Q = "//trim(coop_num2str(co%Q_COUPLING, "(G10.3)")) )
+     end select
+
+  enddo
+  call coop_asy_legend(fp, x = fp%xmin*0.6+fp%xmax*0.4, y = fp%ymax*0.2 + fp%ymin*0.8)
+  call coop_asy_label(fp, label = "$V(\phi) = C_0 + C_1 \phi^{-"//trim(coop_num2str(co%N_POWER))//"}$", x = fp%xmin*0.8+fp%xmax*0.2, y = fp%ymax*0.9 + fp%ymin*0.1)
   call fp%close()
 
 contains
