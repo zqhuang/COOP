7ef20ed29861c1ed7524963dd0d184abc1b1a866
Zhiqi Huang <zhiqihuang@gmail.com>
2014-06-10 19:31:10 -0400

    added auto_update script, updated INSTALL file
    added auto_update , updated INSTALL file

---
+++ autoupdate.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+mv configure.in configure_local.in
+mv test/test.f90 test/test_local.f90
+mv test/example1.f90 test/example1_local.f90
+mv test/example2.f90 test/example2_local.f90
+git pull origin master
+./clear.sh
+./compile.sh
