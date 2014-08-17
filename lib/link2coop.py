fe4a2823c7fb6d78c4525a1ee43d66464b0068aa
Zhiqi Huang <zhiqihuang@gmail.com>
2014-08-16 23:49:07 -0400

    use dynamic allocate
    use dynamic allocate

--- lib/link2coop.py
+++ lib/link2coop.py
@@ -2,6 +2,7 @@
 import re
 import os
 import sys
+import string
 coop_path = r"/mnt/raid-cita/zqhuang/raid-zqhuang/zqhuang/scilibs/COOP"
 
 #######################################################
@@ -102,17 +103,20 @@ def search_value(fname, pattern):
     else:
         return ""
 
-def which_line(fname, pattern):
+def paramnames_list(fname):
+    params = []
     fp = open(fname, 'r')
-    i = 0
     for line in fp:
-        i += 1
-        m = re.search(pattern, line, flags = re.I)
-        if m:
-            fp.close()
-            return i 
+        words = string.split(line)
+        params.append(words[0])
     fp.close()
-    return i
+    return params
+
+def which_line(params, pattern):
+    for i in range(len(params)):
+        if(params[i] == pattern):
+            return i+1 
+    return 0
 
 def last_line(fname, patterns):
     fp = open(fname, 'r')
@@ -160,25 +164,38 @@ if(nstr == ""):
     sys.exit()
 numhard = int( nstr )
 
-index_w = which_line("params_CMB.paramnames", r'^w\s+.+')
+if(os.path.isfile('params_CMB.paramnames')):
+    param_list = paramnames_list("params_CMB.paramnames")
+else:
+    print "cannot find the default paramnames file: params_CMB.paramnames"
+    sys.exit()
+
+index_w = which_line(param_list, r'w')
 if index_w == 0:
     print "cannot determine the index of dark energy parameter"
     sys.exit()
 
-index_nnu = which_line("params_CMB.paramnames", r'^nnu\s+.+')
-if index_nnu == 0:
-    print "cannot determine the index of neutrino numbers"
+index_wa = which_line(param_list, r'wa')
+if index_wa == 0:
+    num_w_params = 1
+else:
+    num_w_params = 2
+
+index_post_w = index_w + num_w_params
+
+index_logA = which_line(param_list, r'logA')
+if index_logA == 0:
+    print "Cannot find logA parameter"
     sys.exit()
 
-index_H0 = which_line("params_CMB.paramnames", r'^H0\*\s+.+')
+if numhard + 1 != index_logA:
+    print "logA is not the first fast parameter"
+    print "This is not compatible with the default settings in COOP"
+    sys.exit()
+
+index_H0 = which_line(param_list, r'H0*')
 if index_H0 == 0:
     print "cannot determine the index of H0*"
-    sys.exit()
-
-
-index_logA = which_line("params_CMB.paramnames", r'^logA\s+.+')
-if numhard + 1 != index_logA:
-    print "Cannot determine the total number of hard parameters"
     sys.exit()
 
 
@@ -195,7 +212,7 @@ print "This version of cosmomc contains:"
 print str(index_H0-1) + "theory parameters"
 print str(numhard) + " slow parameters"
 print str(index_H0-index_logA) + " fast parameters"
-print str(index_nnu-index_w) + " dark energy parameters"
+print str(num_w_params) + " dark energy parameters"
 print "*****************************************"
 print "Modifying files:"
 
@@ -239,9 +256,93 @@ replace_first("source/CosmologyTypes.f90", [line_pattern(r'integer,parameter::ma
 
 replace_first("source/settings.f90", [line_pattern(r'integer,parameter::max_theory_params=\d+')], [r'integer, parameter:: max_theory_params = 50 \n character(LEN=256)::cosmomc_paramnames = "params_CMB.paramnames" '])
 
-replace_all("camb/halofit_ppf.f90", [r'^(\s*subroutine\s+NonLinear_GetNonLinRatios\s*\(\s*CAMB\_Pk\)\s*(\!.*)?)$', r'^\s*om\_m\s*\=\s*omega\_m\s*\(.*$', r'^\s*om\_v\s*\=\s*omega\_v\s*\(.*$', r'\(\s*1\.?\s*\+\s*w\_lam\s*\)'], [r'\1\n use coop_wrapper', r'  om_m = (coop_global_cdm%Omega * coop_global_cdm%rhoa4_ratio(a) + coop_global_baryon%Omega * coop_global_baryon%rhoa4_ratio(a))/coop_global_cosmology%H2a4(a)',  r' om_v = coop_global_de%Omega * coop_global_de%rhoa4_ratio(a) / coop_global_cosmology%H2a4(a) ' , r' coop_global_de%wp1 ' ])
+replace_all("camb/halofit_ppf.f90",  \
+            [ r'^(\s*subroutine\s+NonLinear_GetNonLinRatios\s*\(\s*CAMB\_Pk\)\s*(\!.*)?)$',  \
+              r'^\s*om\_m\s*\=\s*omega\_m\s*\(.*$', r'^\s*om\_v\s*\=\s*omega\_v\s*\(.*$', \
+              r'\(\s*1\.?\s*\+\s*w\_lam\s*\)' ],  \
+            [ r'\1\n use coop_wrapper', \
+              r'  om_m = (coop_global_cdm%Omega * coop_global_cdm%rhoa4_ratio(a) + coop_global_baryon%Omega * coop_global_baryon%rhoa4_ratio(a))/coop_global_cosmology%H2a4(a)',  \
+              r' om_v = coop_global_de%Omega * coop_global_de%rhoa4_ratio(a) / coop_global_cosmology%H2a4(a) ', \
+              r' coop_global_de%wp1 ' ])
 
-replace_all("source/CosmologyParameterizations.f90", [line_pattern(r'module cosmologyparameterizations'), r'H0\_min\s*=\s*\d+', r'H0\_max\s*=\s*\d+', r"call\s+ini\%read\(\s*\'H0\_min\'\s*\,\s*this\%H0\_min\s*\)", r"call\s+ini\%read\(\s*\'H0\_max\'\s*\,\s*this\%H0\_max\s*\)", r'call\s+this\%SetTheoryParameterNumbers\s*\(\s*\d+\,\s*last_power_index\s*\)', r'end\s+subroutine\s+setforH', r'^\s*CMB\%nnu\s*=\s*Params\(\d+\)\s*(\!.*)?$', r'^\s*CMB\%YHe\s*=\s*params\(\d+\)\s*(\!.*)?$', r'^\s*CMB\%iso\_cdm\_correlated\s*=\s*params\(\d+\)\s*(\!.*)?$', r'^\s*CMB\%zre\_delta\s*=\s*params\(\d+\)\s*(\!.*)?$', r'^\s*CMB\%Alens\s*=\s*params\(\d+\)\s*(\!.*)?$',  r'^\s*CMB\%Alensf\s*=\s*params\(\d+\)\s*(\!.*)?$',  r'^\s*CMB\%fdm\s*=\s*params\(\d+\)\s*(\!.*)?$', r'^\s*call\s+setfast\s*\(\s*Params\s*\,\s*CMB\s*\)\s*(\!.*)?$', r"(\'|\")params_CMB.paramnames(\'|\")"], [r'#include "constants.h"\nmodule CosmologyParameterizations\n use coop_wrapper', r'H0_min = 55.', r'H0_max = 85.', r'this%H0_min = 55.', r'this%H0_max = 85.', r'call this%SetTheoryParameterNumbers(cosmomc_de_index + cosmomc_de_num_params+cosmomc_de2pp_num_params-1, cosmomc_pp_num_params)', r'call coop_setup_cosmology_from_cosmomc(params, H0/100.d0)\nend subroutine setForH', r'CMB%nnu = Params(cosmomc_de_index + cosmomc_de_num_params)', r'CMB%YHe = Params(cosmomc_de_index + cosmomc_de_num_params+1)',  r'CMB%iso_cdm_correlated = Params(cosmomc_de_index + cosmomc_de_num_params+2)', r'CMB%zre_delta = Params(cosmomc_de_index + cosmomc_de_num_params+3)', r'CMB%Alens = Params(cosmomc_de_index + cosmomc_de_num_params+4)', r'CMB%Alensf = Params(cosmomc_de_index + cosmomc_de_num_params+5)', r'CMB%fdm = Params(cosmomc_de_index + cosmomc_de_num_params+6)', r"call setfast(params, CMB)\n call coop_setup_cosmology_from_cosmomc(params)\n call coop_setup_pp()", r"adjustl(trim(cosmomc_paramnames))" ])
+list_param_pattern = [line_pattern(r'module cosmologyparameterizations'), \
+                      r'H0\_min\s*=\s*\d+', \
+                      r'H0\_max\s*=\s*\d+', \
+                      r"call\s+ini\%read\(\s*\'H0\_min\'\s*\,\s*this\%H0\_min\s*\)", \
+                      r"call\s+ini\%read\(\s*\'H0\_max\'\s*\,\s*this\%H0\_max\s*\)", \
+                      r'call\s+this\%SetTheoryParameterNumbers\s*\(\s*\d+\,\s*last_power_index\s*\)', \
+                      r'end\s+subroutine\s+setforH',  \
+                      r'^\s*call\s+setfast\s*\(\s*Params\s*\,\s*CMB\s*\)\s*(\!.*)?$', \
+                      r"(\'|\")params_CMB.paramnames(\'|\")"]
+
+
+
+list_param_replace = [r'#include "constants.h"\nmodule CosmologyParameterizations\n use coop_wrapper', \
+                      r'H0_min = 55.', \
+                      r'H0_max = 85.', \
+                      r'this%H0_min = 55.', \
+                      r'this%H0_max = 85.', \
+                      r'call this%SetTheoryParameterNumbers(cosmomc_de_index + cosmomc_de_num_params+cosmomc_de2pp_num_params-1, cosmomc_pp_num_params)', \
+                      r'call coop_setup_cosmology_from_cosmomc(params, H0/100.d0)\nend subroutine setForH', \
+                      r"call setfast(params, CMB)\n call coop_setup_cosmology_from_cosmomc(params)\n call coop_setup_pp()", \
+                      r"adjustl(trim(cosmomc_paramnames))" ]
+
+
+this_index = which_line(param_list, "nnu") 
+if(this_index != 0):
+    list_param_pattern.append(r'^\s*CMB\%nnu\s*=\s*Params\(\d+\)\s*(\!.*)?$')
+    list_param_replace.append(r'CMB%nnu = Params(cosmomc_de_index + cosmomc_de_num_params + ' + str(this_index - index_post_w)  + r' )')
+
+
+
+this_index = which_line(param_list, "") 
+if(this_index != 0):
+    list_param_pattern.append(r'')
+    list_param_replace.append(r' = Params(cosmomc_de_index + cosmomc_de_num_params + ' + str(this_index - index_post_w)  + r' )')
+
+
+this_index = which_line(param_list, "") 
+if(this_index != 0):
+    list_param_pattern.append(r'')
+    list_param_replace.append(r' = Params(cosmomc_de_index + cosmomc_de_num_params + ' + str(this_index - index_post_w)  + r' )')
+
+this_index = which_line(param_list, "yhe") 
+if(this_index != 0):
+    list_param_pattern.append(r'^\s*CMB\%YHe\s*=\s*params\(\d+\)\s*(\!.*)?$')
+    list_param_replace.append(r' CMB%YHe = Params(cosmomc_de_index + cosmomc_de_num_params + ' + str(this_index - index_post_w)  + r' )')
+
+
+this_index = which_line(param_list, "alpha1") 
+if(this_index != 0):
+    list_param_pattern.append(r'^\s*CMB\%iso\_cdm\_correlated\s*=\s*params\(\d+\)\s*(\!.*)?$')
+    list_param_replace.append(r'CMB%iso_cdm_correlated = Params(cosmomc_de_index + cosmomc_de_num_params + ' + str(this_index - index_post_w)  + r' )')
+
+
+this_index = which_line(param_list, "deltazrei") 
+if(this_index != 0):
+    list_param_pattern.append(r'^\s*CMB\%zre\_delta\s*=\s*params\(\d+\)\s*(\!.*)?$')
+    list_param_replace.append(r'CMB%zre_delta = Params(cosmomc_de_index + cosmomc_de_num_params + ' + str(this_index - index_post_w)  + r' )')
+
+
+this_index = which_line(param_list, "Alens") 
+if(this_index != 0):
+    list_param_pattern.append(r'^\s*CMB\%Alens\s*=\s*params\(\d+\)\s*(\!.*)?$')
+    list_param_replace.append(r'CMB%Alens = Params(cosmomc_de_index + cosmomc_de_num_params + ' + str(this_index - index_post_w)  + r' )')
+
+
+this_index = which_line(param_list, "Alensf") 
+if(this_index != 0):
+    list_param_pattern.append(r'^\s*CMB\%Alensf\s*=\s*params\(\d+\)\s*(\!.*)?$')
+    list_param_replace.append(r'CMB%Alensf = Params(cosmomc_de_index + cosmomc_de_num_params + ' + str(this_index - index_post_w)  + r' )')
+
+this_index = which_line(param_list, "fdm") 
+if(this_index != 0):
+    list_param_pattern.append( r'^\s*CMB\%fdm\s*=\s*params\(\d+\)\s*(\!.*)?$')
+    list_param_replace.append(r'CMB%fdm = Params(cosmomc_de_index + cosmomc_de_num_params + ' + str(this_index - index_post_w)  + r' )')
+
+
+
+replace_all("source/CosmologyParameterizations.f90", list_param_pattern, list_param_replace )
 
 replace_first("source/driver.F90", [line_pattern(r'program cosmomc'), line_pattern(r'call ini%open(inputfile)')], [r'program cosmomc \n  use coop_wrapper', r'call Ini%Open(InputFile)\n cosmomc_de_model = Ini%Read_Int("de_model", cosmomc_de_model) \n cosmomc_de_num_params = Ini%Read_Int("de_num_params", cosmomc_de_num_params)\n cosmomc_pp_model = Ini%Read_Int("pp_model", cosmomc_pp_model) \n cosmomc_pp_num_params = Ini%Read_Int("pp_num_params", cosmomc_pp_num_params) \n cosmomc_pp_inflation_consistency = Ini%Read_Logical("inflation_consistency", .true.) \n cosmomc_paramnames = Ini%Read_String("paramnames", .false.) \n if(trim(cosmomc_paramnames).eq."") stop "cannot find the paramnames entry in the ini file" \n cosmomc_de_index = ' + str(index_w) + r'\n cosmomc_de2pp_num_params = ' + str(numhard - 1) + r' - cosmomc_de_index \n cosmomc_pp_num_origin = last_power_index'])
 
@@ -251,19 +352,22 @@ print "Generating params files"
 
 os.system('mkdir paramnames')
 os.system('cp params_CMB.paramnames paramnames/')
-copy_replace_first("params_CMB.paramnames", "paramnames/params_qcdm.paramnames", [r'^w\s+.+$', r'^wa\s+.+$'], [r"epss          \epsilon_s", r"epsinf           \epsilon_{\infty}\nzetas           \zeta_s\natbyaeq              a_t/a_{\\rm eq}\nQeq                  Q_{\\rm eq} \ndlnQdphi           d\\ln Q/d\\phi" ] )
+if(num_w_params == 2):
+    copy_replace_first("params_CMB.paramnames", "paramnames/params_qcdm.paramnames", [r'^w\s+.+$', r'^wa\s+.+$'], [r"epss          \epsilon_s", r"epsinf           \epsilon_{\infty}\nzetas           \zeta_s\natbyaeq              a_t/a_{\\rm eq}\nQeq                  Q_{\\rm eq} \ndlnQdphi           d\\ln Q/d\\phi" ] )
+else:
+    copy_replace_first("params_CMB.paramnames", "paramnames/params_qcdm.paramnames", [r'^w\s+.+$'], [r"epss          \epsilon_s\nepsinf           \epsilon_{\infty}\nzetas           \zeta_s\natbyaeq              a_t/a_{\\rm eq}\nQeq                  Q_{\\rm eq} \ndlnQdphi           d\\ln Q/d\\phi" ] )
 
 
 ##lcdm
-copy_replace_first("test.ini", 'lcdm.ini', [common_pattern, r'^file_root\s*=.+$', r'^action\s*=.+$',  propose_pattern], [r'\1 \nde_model = 2 \nde_num_params='+str(index_nnu-index_w)+r'\npp_model = 0 \npp_num_params = ' + str(index_H0 - index_logA) +r'\nparamnames = paramnames/params_CMB.paramnames', r'file_root = lcdm', r'action = 0', str_propose] )
+copy_replace_first("test.ini", 'lcdm.ini', [common_pattern, r'^file_root\s*=.+$', r'^action\s*=.+$',  propose_pattern], [r'\1 \nde_model = 2 \nde_num_params='+str(num_w_params)+r'\npp_model = 0 \npp_num_params = ' + str(index_H0 - index_logA) +r'\nparamnames = paramnames/params_CMB.paramnames', r'file_root = lcdm', r'action = 0', str_propose] )
 
 
 ### w0
-copy_replace_first("test.ini", 'w0.ini', [common_pattern,  r'^file_root\s*=.+$', r'^action\s*=.+$', propose_pattern], [r'DEFAULT(' + batch_dir + r'/common_w0.ini) \nde_model = 2 \nde_num_params='+str(index_nnu-index_w)+r'\npp_model = 0 \npp_num_params = ' + str(index_H0 - index_logA) +r'\nparamnames = paramnames/params_CMB.paramnames', r'file_root = w0', r'action = 0', str_propose] )
+copy_replace_first("test.ini", 'w0.ini', [common_pattern,  r'^file_root\s*=.+$', r'^action\s*=.+$', propose_pattern], [r'DEFAULT(' + batch_dir + r'/common_w0.ini) \nde_model = 2 \nde_num_params='+str(num_w_params)+r'\npp_model = 0 \npp_num_params = ' + str(index_H0 - index_logA) +r'\nparamnames = paramnames/params_CMB.paramnames', r'file_root = w0', r'action = 0', str_propose] )
 copy_replace_first(common_file, batch_dir + r'/common_w0.ini', [r'^INCLUDE\(params_CMB_defaults\.ini\)\s*$'], [r'INCLUDE(params_CMB_w0.ini)'] )
 copy_replace_first(batch_dir + r'/params_CMB_defaults.ini', batch_dir + r'/params_CMB_w0.ini', [r'^param\[w\]\s*=.+$', r'^param\[wa\]\s*=.+$'], [ r'param[w] = -1 -3 1 0.05 0.05', r'param[wa] = 0 0 0 0 0' ] ) 
 
-copy_replace_first("test.ini", 'w0wa.ini', [common_pattern, r'^file_root\s*=.+$', r'^action\s*=.+$',  propose_pattern], [r'DEFAULT(' + batch_dir + r'/common_w0wa.ini) \nde_model = 2 \nde_num_params='+str(index_nnu-index_w)+r'\npp_model = 0 \npp_num_params = ' + str(index_H0 - index_logA) +r'\nparamnames = paramnames/params_CMB.paramnames', r'file_root = w0wa', r'action = 0', str_propose] )
+copy_replace_first("test.ini", 'w0wa.ini', [common_pattern, r'^file_root\s*=.+$', r'^action\s*=.+$',  propose_pattern], [r'DEFAULT(' + batch_dir + r'/common_w0wa.ini) \nde_model = 2 \nde_num_params='+str(num_w_params)+r'\npp_model = 0 \npp_num_params = ' + str(index_H0 - index_logA) +r'\nparamnames = paramnames/params_CMB.paramnames', r'file_root = w0wa', r'action = 0', str_propose] )
 copy_replace_first(common_file, batch_dir + r'/common_w0wa.ini', [r'^INCLUDE\(params_CMB_defaults\.ini\)\s*$'], [r'INCLUDE(params_CMB_w0wa.ini)'] )
 copy_replace_first(batch_dir + r'/params_CMB_defaults.ini', batch_dir + r'/params_CMB_w0wa.ini', [r'^param\[w\]\s*=.+$', r'^param\[wa\]\s*=.+$'], [ r'param[w] = -1 -3 1 0.05 0.05', r'param[wa] = 0. -3 3 0.2 0.2' ] ) 
 
