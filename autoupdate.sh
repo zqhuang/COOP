cb188622e54a452e97758da7eacc291e45922ac2
Zhiqi Huang <zhiqihuang@gmail.com>
2014-09-25 17:38:57 -0400

    change auto update shell

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 mv configure.in configure_local.in
-mv test/test.f90 test/test_local.f90
-mv test/example1.f90 test/example1_local.f90
-mv test/example2.f90 test/example2_local.f90
+git stash save --keep-index
 git pull origin master
 mv configure_local.in configure.in
 ./clear.sh
