80f172b355e5388bb812a698012c33014952d0a5
Zhiqi Huang <zhiqihuang@gmail.com>
2014-10-28 02:03:13 -0400

    now do HA
    now do HA

--- utils/test.f90
+++ utils/test.f90
@@ -4,15 +4,13 @@ program Test
 #include "constants.h"
   type(coop_asy)::fig
   COOP_INT, parameter::n=100
-  COOP_REAL::x(n), yb(n), yt(n)
-  call coop_set_uniform(n, x, 0.d0, coop_2pi)
-  yb = sin(x)-0.1d0
-  yt = sin(x) + cos(x)**2/2.d0
-  call fig%open("testband.txt")
-  call fig%init(xlabel = "x", ylabel="y")
-  call coop_asy_band(fp = fig, x = x, ylower = yb, yupper = yt, colorfill = "lightgray")
-  yb = sin(x)-0.05d0
-  yt = sin(x) + cos(x)**2/10.d0
-  call coop_asy_band(fp = fig, x = x, ylower = yb, yupper = yt, colorfill = "gray")
-  call fig%close()
+  COOP_INT i
+  COOP_REAL::x(n), y(n),yapp(n)
+  call coop_set_uniform(n, x, 0.d0, coop_SI_degree*5.d0*60.d0)
+  yapp = 1.d0+x**2/4.d0+x**4/64.d0
+  do i=1, n
+     y(i) = coop_BessJ0(x(i))
+     write(*,*) x(i), y(i), yapp(i)
+  enddo
+
 end program Test
