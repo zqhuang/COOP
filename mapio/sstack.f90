80494802a2acc03b1cd6b77c9f174873cae98d8a
Zhiqi Huang <zhiqihuang@gmail.com>
2014-10-31 01:37:04 -0400

    sstack

--- mapio/sstack.f90
+++ mapio/sstack.f90
@@ -15,9 +15,11 @@ program hastack_prog
   COOP_UNKNOWN_STRING, parameter::stack_type = "QrUr"
   COOP_REAL, parameter::patch_size = 2.d0*coop_SI_degree
 
-  COOP_UNKNOWN_STRING, parameter::prefix = "ha_r2f30n1024/"
+  
+
+  COOP_UNKNOWN_STRING, parameter::prefix = "sst_r2f15n1024/"
   COOP_INT, parameter::mmax = 4
-  COOP_REAL, parameter::fwhm_arcmin = 30.d0
+  COOP_REAL, parameter::fwhm_arcmin = 15.d0
   COOP_REAL, parameter::fwhm_in = 10.d0
   COOP_UNKNOWN_STRING, parameter::postfix =   "_010a_1024.fits"
 
@@ -25,7 +27,7 @@ program hastack_prog
   COOP_UNKNOWN_STRING, parameter::mapdir = "/mnt/scratch-lustre/zqhuang/scratch-3month/zqhuang/"
   COOP_REAL,parameter::fwhm = coop_SI_arcmin * sqrt(fwhm_arcmin**2-fwhm_in**2)
   COOP_REAL, parameter::threshold = 0
-  COOP_REAL, parameter::dr = coop_SI_arcmin * max(fwhm_arcmin/4.d0, fwhm_in)
+  COOP_REAL, parameter::dr = coop_SI_arcmin * max(fwhm_arcmin/4.d0, 5.d0)
   COOP_INT, parameter::n = nint(patch_size/dr)
 
   COOP_UNKNOWN_STRING, parameter::imap_file  = "planck14/dx11_v2_smica_int_cmb"//postfix
@@ -34,55 +36,33 @@ program hastack_prog
   COOP_UNKNOWN_STRING, parameter::polmask_file  ="planck14/dx11_v2_smica_pol_mask"//postfix
 
   type(coop_healpix_maps)::polmask, imask, noise, imap, polmap, tmpmap
-  type(coop_healpix_patch)::patch_s, patch_n
-  integer,parameter::scan_nside = 4
-    integer,parameter::scan_npix = scan_nside**2*6
-  integer run_id, i, ind, j
-  COOP_REAL   hdir(2)
+  type(coop_healpix_patch)::patch
+  COOP_INT i, j, ind
   COOP_STRING::fr_file
   type(coop_list_integer)::listpix
   type(coop_list_real)::listangle
   type(coop_file) fp
   call coop_MPI_init()
-  if(iargc() .ge. 1)then
-     run_id = coop_str2int(coop_InputArgs(1))
-  else
-     run_id = coop_MPI_Rank()
-  endif
-  call sleep(run_id*5)  !!sleep for 5 seconds so that files are not read simultaneously
-  if(run_id .ge.  scan_npix)then
-     write(*,*) "run id must not exceed ", scan_nside**2*12 - 1
-     call coop_MPI_Abort()
-  endif
-  call pix2ang_ring(scan_nside, run_id, hdir(1), hdir(2))
-
-  !!read masks
   call imask%read(imask_file, nmaps_wanted = 1, spin = (/ 0 /) )
   if(stack_type .ne. "T")then
      call polmask%read(polmask_file, nmaps_wanted = 1, spin = (/ 0 /) )
   endif
-
   
-  call patch_n%init(stack_type, n, dr, mmax = mmax)
-  patch_s = patch_n
+  call patch%init(stack_type, n, dr, mmax = mmax)
 
-  allprefix = prefix//stack_type//"_on_"//spot_type//"_fr_"//COOP_STR_OF(scan_nside)//"_"
+  allprefix = prefix//stack_type//"_on_"//spot_type//"_fr"
 
-  if(run_id.eq.0)then
-     call fp%open(trim(allprefix)//"_info.txt", "w")
-     write(*,*) n, patch_n%nmaps, dr/coop_SI_arcmin
-     call fp%close()
-  endif
-  fr_file = trim(allprefix)//COOP_STR_OF(run_id)//".dat"
+  call fp%open(trim(allprefix)//"_info.txt", "w")
+  write(*,*) n, patch%nmaps, dr/coop_SI_arcmin
+  call fp%close()
 
-
+  fr_file = trim(allprefix)//".dat"
   ind = -1
   if(.not. coop_file_exists(trim(fr_file)))goto 200
   call fp%open(trim(fr_file), "ru")
   do
      read(fp%unit, ERR=100, END=100) i
-     read(fp%unit, ERR=100, END=100) patch_n%fr
-     read(fp%unit, ERR=100, END=100) patch_s%fr
+     read(fp%unit, ERR=100, END=100) patch%fr
      if(i.ne.ind+1) call cooP_MPI_Abort("fr file broken")
      ind = i
      if(ind .ge. n_sim) exit
@@ -92,8 +72,7 @@ program hastack_prog
 200 call fp%open(trim(fr_file), "u")
   do i=0, ind
      read(fp%unit, ERR=100, END=100) j
-     read(fp%unit, ERR=100, END=100) patch_n%fr
-     read(fp%unit, ERR=100, END=100) patch_s%fr
+     read(fp%unit, ERR=100, END=100) patch%fr
      if(j.ne.i)call cooP_MPI_Abort("fr file broken")
   enddo
   do while(ind .lt. n_sim)
@@ -103,18 +82,15 @@ program hastack_prog
      case("T")
         noise%map = imap%map
         call noise%get_listpix(listpix, listangle, spot_type, threshold, imask)
-        call imap%stack_north_south(patch_n, patch_s, listpix, listangle, hdir, imask)
+        call imap%stack_with_listpix(patch, listpix, listangle, imask)
      case default
         call imap%get_listpix(listpix, listangle, spot_type, threshold, imask)
         call load_polmap(ind)
-        call polmap%stack_north_south(patch_n, patch_s, listpix, listangle, hdir, polmask)
+        call polmap%stack_with_listpix(patch, listpix, listangle, polmask)
      end select
-
-     call patch_n%get_all_radial_profiles()
-     call patch_s%get_all_radial_profiles()
+     call patch%get_all_radial_profiles()
      write(fp%unit) ind
-     write(fp%unit) patch_n%fr
-     write(fp%unit) patch_s%fr
+     write(fp%unit) patch%fr
      flush(fp%unit)
   enddo
   call fp%close()
