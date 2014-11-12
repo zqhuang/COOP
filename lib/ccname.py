a8591f65dc507a520a142ea1b4775f70e43e5771
Zhiqi Huang <zhiqihuang@gmail.com>
2014-11-12 12:15:08 -0500

    change ccname

--- lib/ccname.py
+++ lib/ccname.py
@@ -10,17 +10,22 @@ plist = [r".converge_stat", r".inputparams", r".paramnames", r".ranges", r".like
 namefrom = "chains/" + sys.argv[1]
 nameto =  "chains/" + sys.argv[2]
 
+def rename_file(ffrom, fto):
+    if(not os.path.isfile(ffrom)):
+        print ffrom + " is missing"
+        return
+    if(os.path.isfile(fto)):
+        print  fto + " already exists"
+        return
+    os.system("mv " + ffrom + " " + fto)
+    return
 
-if os.path.isfile(nameto + ".inputparams"):
-    print "the target name already exists"
-    sys.exit()
 
 for postfix in plist:
-    if(os.path.isfile( namefrom + postfix)):
-        os.system("mv " + namefrom + postfix + " " + nameto + postfix)
+    rename_file(namefrom + postfix, nameto + postfix)
 
 for i in range(1, numchains+1):
-    os.system("mv " + namefrom + "_" + str(i) + ".txt " + nameto + "_" + str(i) + ".txt")
-    os.system("mv " + namefrom + "_" + str(i) + ".log " + nameto + "_" + str(i) + ".log")
-    if(os.path.isfile(namefrom + "_" + str(i) + ".chk ")):
-        os.system("mv " + namefrom + "_" + str(i) + ".chk " + nameto + "_" + str(i) + ".chk")
+    rename_file(namefrom + "_" + str(i) + ".txt", nameto + "_" + str(i) + ".txt")
+    rename_file(namefrom + "_" + str(i) + ".log", nameto + "_" + str(i) + ".log")
+    rename_file(namefrom + "_" + str(i) + ".chk", nameto + "_" + str(i) + ".chk")
+
