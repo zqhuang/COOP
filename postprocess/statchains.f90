48636f9921618ab24996cae2acba277167c2d835
Zhiqi Huang <zhiqihuang@gmail.com>
2014-11-23 20:04:03 -0500

    confused

--- postprocess/statchains.f90
+++ postprocess/statchains.f90
@@ -461,7 +461,7 @@ contains
     integer,parameter::distlss = 13893.
     integer, parameter::num_1sigma_trajs = 50
     integer, parameter::num_samples_to_get_mean = 2500
-    integer,parameter::lmin = coop_pp_lmin, lmax = coop_pp_lmax, num_cls_samples = 3
+    integer,parameter::lmin = coop_pp_lmin, lmax = coop_pp_lmax, num_cls_samples = 10
     COOP_REAL,parameter::standard_ns = 0.968d0
     COOP_REAL,parameter::low_ell_cut = 50
     COOP_REAL,parameter::low_k_cut = low_ell_cut/distlss
