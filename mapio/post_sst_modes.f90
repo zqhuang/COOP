67e09f872387ba8cb4b420804d8d01ffc584ae2a
Zhiqi Huang <zhiqihuang@gmail.com>
2014-11-24 12:19:25 -0500

    random traps

--- mapio/post_sst_modes.f90
+++ mapio/post_sst_modes.f90
@@ -116,8 +116,16 @@ program test
         call fig%band(r, bounds(-2,:)-bounds(0,:), bounds(2,:)-bounds(0,:), colorfill = trim(coop_asy_gray_color(0.65)), linecolor = "invisible")
         call fig%band(r, bounds(-1,:)-bounds(0,:), bounds(1,:)-bounds(0,:), colorfill = trim(coop_asy_gray_color(0.36)), linecolor = "invisible")        
         call fig%curve(r, vec(:, 0) - bounds(0, :), color = "red", linetype = "solid", linewidth = 1.5, legend = "Planck")
-        call fig%add_legend(legend = "FFP8 1-$\sigma$", color = trim(coop_asy_gray_color(0.36)))
-        call fig%add_legend(legend = "FFP8 2-$\sigma$", color = trim(coop_asy_gray_color(0.65)))        
+        do i=1, 6
+           j = coop_random_index(nsims)
+           if(i.eq.1)then
+              call fig%curve(r, vec(:, j) - bounds(0, :), color = "skyblue", linetype = "dotted", linewidth = 1., legend = "FFP8 sims.")
+           else
+              call fig%curve(r, vec(:, j) - bounds(0, :), color = "skyblue", linetype = "dotted", linewidth = 1.)
+           endif
+        enddo
+        call fig%add_legend(legend = "1-$\sigma$ bound", color = trim(coop_asy_gray_color(0.36)))
+        call fig%add_legend(legend = "2-$\sigma$ bound", color = trim(coop_asy_gray_color(0.65)))        
         call fig%expand(0., 0., 0.01, 0.1)
         call fig%legend(0.05, 0.96, 1)
         call fig%close()
