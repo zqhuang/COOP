ec92d9f92921c38c05e2fe52706f140c70a9857d
Zhiqi Huang <zhiqihuang@gmail.com>
2014-11-24 20:19:32 -0500

    mistake

--- postprocess/getdist.f90
+++ postprocess/getdist.f90
@@ -50,7 +50,7 @@ program getdist
   measured_cltt_file = ini_read_string("measured_cltt_file", .false.)
   bestfit_cl_file = ini_read_string("bestfit_cl_file", .false.)
   bestfit_run_file = ini_read_string("bestfit_run_file", .false.)
-  bestfit_run_file = ini_read_string("bestfit_varytau_file", .false.)
+  bestfit_varytau_file = ini_read_string("bestfit_varytau_file", .false.)
   call load_chain(mc, prefix, discard_percent)
 
   inline = trim(adjustl(ini_read_string("want2d", .false.)))
