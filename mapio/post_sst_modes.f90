37557e7807e776bba53a5cc2382a11a0ff68bceb
Zhiqi Huang <zhiqihuang@gmail.com>
2014-11-26 01:39:16 -0500

    change color back

--- mapio/post_sst_modes.f90
+++ mapio/post_sst_modes.f90
@@ -125,7 +125,7 @@ program test
               call fig%curve(r, vec(:, j) - bounds(0, :), color = "HEX:1010EF", linetype = "dotted", linewidth = 1.)
            endif
         enddo
-        call fig%curve(r, vec(:, 0) - bounds(0, :), color = "HEX:21C0FC", linetype = "solid", linewidth = 1.5, legend = "Planck")        
+        call fig%curve(r, vec(:, 0) - bounds(0, :), color = "red", linetype = "solid", linewidth = 1.5, legend = "Planck")        
         call fig%add_legend(legend = "1-$\sigma$ bound", color = trim(coop_asy_gray_color(0.42)))
         call fig%add_legend(legend = "2-$\sigma$ bound", color = trim(coop_asy_gray_color(0.65)))        
         call fig%expand(0., 0., 0.01, 0.25)
@@ -137,7 +137,7 @@ program test
         call fig%band(r, bounds(-2,:), bounds(2,:), colorfill = trim(coop_asy_gray_color(0.65)), linecolor = "invisible")
         call fig%band(r, bounds(-1,:), bounds(1,:), colorfill = trim(coop_asy_gray_color(0.42)), linecolor = "invisible")        
         call fig%curve(r, bounds(0,:), color = "red", linetype = "solid", linewidth = 1., legend = "mean")                
-        call fig%curve(r, vec(:, 0), color = "HEX:21C0FC", linetype = "solid", linewidth = 1., legend = "Planck")        
+        call fig%curve(r, vec(:, 0), color = "red", linetype = "solid", linewidth = 1., legend = "Planck")        
         call fig%add_legend(legend = "1-$\sigma$ bound", color = trim(coop_asy_gray_color(0.42)))
         call fig%add_legend(legend = "2-$\sigma$ bound", color = trim(coop_asy_gray_color(0.65)))        
         call fig%expand(0., 0., 0.01, 0.25)
