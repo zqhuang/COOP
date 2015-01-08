25b4fbc1caadace4dbb5e3a13928e6f6de8886c7
Zhiqi Huang <zhiqihuang@gmail.com>
2015-01-07 19:12:52 -0500

    tiwsts

--- mapio/getcls.f90
+++ mapio/getcls.f90
@@ -12,14 +12,18 @@ program test
 #include "constants.h"
 
   COOP_STRING::imap_file, polmap_file, output_file
-  COOP_INT, parameter::lmax = 2000
+  COOP_INT, parameter::lmax = 3000
   COOP_INT, parameter::smooth_delta_ell = 20
   
   COOP_UNKNOWN_STRING, parameter::imask_file = "planck14/dx11_v2_common_int_mask_010a_1024.fits"
   COOP_UNKNOWN_STRING, parameter::polmask_file = "planck14/dx11_v2_common_pol_mask_010a_1024.fits"
+  COOP_REAL, parameter::rmin = coop_ln2, rmax = log(dble(lmax))
   type(coop_healpix_maps)::map, imask, polmask
   type(coop_file)::fp
   integer l, i
+  COOP_INT, parameter::fit_n = 8
+  COOP_REAL::coef(fit_n, 3)
+
   if(iargc().lt.2)then
      write(*,*) "./GetCl imap polmap"
      write(*,*) " or"
@@ -35,8 +39,20 @@ program test
   endif
   call map%read(imap_file, nmaps_wanted = 3, spin = (/ 0, 2, 2 /) )
   call map%import(polmap_file, index_start = 2, index_end = 3, spin = (/ 2, 2 /))
+
+  call imask%read(imask_file, nmaps_wanted = 1, spin = (/ 0 /) )
+  call polmask%read(polmask_file, nmaps_wanted = 1, spin = (/ 0 /) )
+  map%map(:,1) = map%map(:, 1)*imask%map(:,1)
+  map%map(:,2) = map%map(:, 2)*polmask%map(:,1)
+  map%map(:,3) = map%map(:, 3)*polmask%map(:,1)  
+  
   call map%map2alm(lmax = lmax)
   call map%get_cls()
+  map%cl(:,1) = map%cl(:,1) * (imask%npix/sum(dble(imask%map(:,1))))
+  map%cl(:,2) = map%cl(:,2) * (polmask%npix/sum(dble(polmask%map(:,1))))
+  map%cl(:,3) = map%cl(:,3) * (polmask%npix/sum(dble(polmask%map(:,1))))
+
+  
   do l=0, map%lmax
      map%cl(l, :) = map%cl(l, :)*(l*(l+1)/coop_2pi*1.e12)
   enddo
