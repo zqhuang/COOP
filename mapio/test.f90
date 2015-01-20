8913fbf672114548ab49461765e8633089b23031
Zhiqi Huang <zhiqihuang@gmail.com>
2015-01-20 16:52:51 -0500

    change test

--- mapio/test.f90
+++ mapio/test.f90
@@ -9,10 +9,5 @@ program stackth
   use alm_tools
   implicit none
 #include "constants.h"
-  type(coop_healpix_maps)::hgm, mask, hgm2
-  COOP_INT l
-  call hgm%init(nside = 256, nmaps = 1, spin = (/ 0 /) )
-  call hgm%mark_spots("spots/dust_TQUL_015a_b30-500_n512_fwhm15_TQUL_threshold0pt5_2ndthreshold0pt6.txt", imap = 1, width = 30.d0, length = 180.d0)
-  call hgm%write("peaks.fits")
-  
+  call coop_healpix_latitutde_cut_mask(512, 30.d0, "planck14/lat30_mask.fits")
 end program stackth
