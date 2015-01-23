02caa4c72eed9e61402176d6a22542de126e11ee
Zhiqi Huang <zhiqihuang@gmail.com>
2015-01-22 19:07:47 -0500

    working version of sst

--- mapio/sstack.f90
+++ mapio/sstack.f90
@@ -6,13 +6,14 @@ program Exp_spots
 #include "constants.h"
 #ifdef HAS_HEALPIX
 
+  COOP_STRING::cc_method 
   
-  COOP_STRING::output = "st/smica_nu0pt1.dat"
-  COOP_REAL::threshold = 0.1d0
+  COOP_STRING::output, line
+  COOP_REAL::threshold
 
-  COOP_INT,parameter::n_sim = 30
-  COOP_STRING::peak_name = "$T$"
-  COOP_STRING::orient_name = "NULL"
+  COOP_INT::n_sim = 30
+  COOP_STRING,parameter::peak_name = "$T$"
+  COOP_STRING::orient_name 
   COOP_STRING::stack_field_name = "T"
   COOP_INT,parameter::n = 36
   COOP_REAL,parameter::r_degree  = 2.d0
@@ -20,12 +21,11 @@ program Exp_spots
 
   COOP_REAL::r(0:n), pfr(0:n), pfr0(0:n)
   COOP_INT::count_p
+  COOP_UNKNOWN_STRING,parameter::outputdir = "st/"  
   COOP_UNKNOWN_STRING,parameter::mapdir = "massffp8/"
   COOP_UNKNOWN_STRING,parameter::postfix = "_020a_0512.fits"
-  COOP_STRING::imap_file = "planck14/dx11_v2_smica_int_cmb"//postfix
-  COOP_STRING::polmap_file = "planck14/dx11_v2_smica_pol_case1_cmb_hp_20_40"//postfix  
-  COOP_STRING::imask_file = "planck14/dx11_v2_common_int_mask"//postfix !"planck14/coldspot_mask_0512.fits"  !
-  COOP_STRING::polmask_file = "planck14/dx11_v2_common_pol_mask"//postfix
+  
+  COOP_STRING::imap_file, polmap_file, imask_file, polmask_file
   
   type(coop_stacking_options)::sto_max, sto_min
   type(coop_healpix_patch)::patch_max, patch_min
@@ -35,8 +35,45 @@ program Exp_spots
   type(coop_file)::fp
   type(coop_asy)::fig
   COOP_INT i
-  COOP_INT ind, ind_done  
-  !!specified for tempearture  
+  COOP_INT ind, ind_done
+  
+  if(iargc() .lt. 3)then
+     write(*,*) "Syntax:"
+     write(*,*) "./SST cc_method nu n_sim [Orient] [cut_cold_spot]"
+     write(*,*) "Examples:"     
+     write(*,*) "./SST smica 0.5 1000"
+     write(*,*) "./SST nilc 0. 100 T T"
+     write(*,*) "./SST commander 2. 100 F T"          
+     stop
+  endif
+  cc_method = trim(coop_inputArgs(1))
+  imap_file = "planck14/dx11_v2_"//trim(cc_method)//"_int_cmb"//postfix
+  polmap_file = "planck14/dx11_v2_"//trim(cc_method)//"_pol_case1_cmb_hp_20_40"//postfix
+  
+  line = coop_inputArgs(2)
+  read(line, *) threshold
+  line = coop_inputArgs(3)
+  read(line,*) n_sim
+
+  if(trim(coop_inputArgs(4)).eq."T")then
+     orient_name = "$(Q_T, U_T)$"
+  else
+     orient_name = "NULL"
+  endif
+  
+  if(trim(coop_inputArgs(5)).eq."T")then
+     imask_file = "planck14/coldspot_mask"//postfix
+     output = trim(outputdir)//trim(cc_method)//"_nu"//trim(coop_num2goodstr(threshold))//"_"//trim(coop_str_numalpha(peak_name))//"_Orient"//trim(coop_str_numalpha(orient_name))//"_CutColdSpot.dat"            
+  else
+     imask_file = "planck14/dx11_v2_common_int_mask"//postfix
+     output = trim(outputdir)//trim(cc_method)//"_nu"//trim(coop_num2goodstr(threshold))//"_"//trim(coop_str_numalpha(peak_name))//"_Orient"//trim(coop_str_numalpha(orient_name))//".dat"                 
+  endif
+  polmask_file = "planck14/dx11_v2_common_pol_mask"//postfix
+  
+  
+
+
+  
   if(trim(orient_name).eq."NULL")then
      call sto_max%init(.true., peak_name, orient_name, nmaps = 1)
      call sto_min%init(.false., peak_name, orient_name, nmaps = 1)     
@@ -60,6 +97,7 @@ program Exp_spots
   
   ind_done = -1  
   call imask%read(imask_file, nmaps_wanted = 1, spin = (/ 0 /) )
+
   if(coop_file_exists(output))then
      call fp%open(output, "ru")
      do i = 0, n_sim
@@ -123,11 +161,11 @@ contains
        call imap%read(imap_file, nmaps_wanted = sto_max%nmaps, nmaps_to_read = 1)
     else
        if(first_load)then
-          call imap%read(trim(mapdir)//"dx11_v2_smica_int_cmb_mc_"//trim(coop_Ndigits(i-1, 5))//trim(postfix), nmaps_to_read = 1, nmaps_wanted = sto_max%nmaps)
-          call inoise%read(trim(mapdir)//"dx11_v2_smica_int_noise_mc_"//trim(coop_Ndigits(i-1, 5))//trim(postfix), nmaps_to_read = 1, nmaps_wanted = sto_max%nmaps)
+          call imap%read(trim(mapdir)//"cmb/dx11_v2_"//trim(cc_method)//"_int_cmb_mc_"//trim(coop_Ndigits(i-1, 5))//trim(postfix), nmaps_to_read = 1, nmaps_wanted = sto_max%nmaps)
+          call inoise%read(trim(mapdir)//"noise/dx11_v2_"//trim(cc_method)//"_int_noise_mc_"//trim(coop_Ndigits(i-1, 5))//trim(postfix), nmaps_to_read = 1, nmaps_wanted = sto_max%nmaps)
        else          
-          call imap%read(trim(mapdir)//"dx11_v2_smica_int_cmb_mc_"//trim(coop_Ndigits(i-1, 5))//trim(postfix), nmaps_to_read = 1, known_size = .true.)
-          call inoise%read(trim(mapdir)//"dx11_v2_smica_int_noise_mc_"//trim(coop_Ndigits(i-1, 5))//trim(postfix), nmaps_to_read = 1, known_size = .true.)
+          call imap%read(trim(mapdir)//"cmb/dx11_v2_"//trim(cc_method)//"_int_cmb_mc_"//trim(coop_Ndigits(i-1, 5))//trim(postfix), nmaps_to_read = 1, known_size = .true.)
+          call inoise%read(trim(mapdir)//"noise/dx11_v2_"//trim(cc_method)//"_int_noise_mc_"//trim(coop_Ndigits(i-1, 5))//trim(postfix), nmaps_to_read = 1, known_size = .true.)
           first_load = .false.
        endif
        imap%map(:, 1) = imap%map(:, 1) + inoise%map(:, 1)       
