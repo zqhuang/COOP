eb5ffb7d8121702f161a0d8c772fdc337478dd67
Zhiqi Huang <zhiqihuang@gmail.com>
2015-04-27 15:32:14 -0400

    plot cl diff for different Q

--- firstorder/test_mult_cls.f90
+++ firstorder/test_mult_cls.f90
@@ -7,11 +7,15 @@ program test
   type(coop_cosmology_firstorder)::fod
   COOP_INT,parameter::lmin = 2, lmax = 2000
   COOP_REAL, dimension(coop_num_Cls, lmin:lmax)::Cls_scalar, Cls_tensor, Cls_lensed
+  COOP_REAL, dimension(coop_num_Cls, lmin:lmax)::Cls_scalar_Q0, Cls_tensor_Q0, Cls_lensed_Q0
   COOP_REAL::ells(lmin:lmax)
   COOP_INT l, ik
   COOP_REAL norm
   type(coop_file)::fp
   type(coop_asy)::fig
+  logical::plot_lensed = .false.
+  logical::logscale = .true.
+  logical::plot_diff = .true.
 
   norm = 2.72558**2*1.d12
 !$  call fod%set_standard_cosmology(Omega_b=0.0485374d0, Omega_c=0.2585497252d0, h = 0.67766d0, tau_re = 0.08193d0, As = 2.2098d-9, ns = 0.968d0, nrun = 0.d0, r = 0.d0, nt = -0.01d0, YHe = 0.248d0, Nnu = 3.d0, de_Q = Q0, de_tracking_n = tracking_n, de_dlnQdphi = 0.d0, de_dUdphi = 0.d0, de_d2Udphi2 = 0.d0 )
@@ -43,9 +47,16 @@ program test
 !!$     write(fp%unit, "(I5, 20E16.7)") l, Cls_scalar(:, l)*(l*(l+1.d0)/coop_2pi*norm)
 !!$  enddo
 !!$  call fp%close()
-
-  call fig%open("Cl_Q.txt")
-  call fig%init(xlabel = "$\ell$", ylabel = "$\frac{\ell(\ell+1)C_{\ell}}{2\pi} (\mu K^2)$ ", xlog = .true., xmin = 1.8, xmax = 2200., caption="Coupled Dark Energy $Q = \frac{\partial \ln m_{DM}}{\partial \phi}$")
+  if(plot_lensed)then
+     call fig%open("Cl_lensed_Q.txt")     
+  else
+     call fig%open("Cl_Q.txt")
+  endif
+  if(plot_diff)then
+     call fig%init(xlabel = "$\ell$", ylabel = "$\frac{\ell(\ell+1)\delta C_{\ell}}{2\pi} (\mu K^2)$ ", xlog = logscale  , xmin =1.8, xmax = 2200., caption="Coupled Dark Energy $Q = \frac{\partial \ln m_{DM}}{\partial \phi}$")     
+  else
+     call fig%init(xlabel = "$\ell$", ylabel = "$\frac{\ell(\ell+1)C_{\ell}}{2\pi} (\mu K^2)$ ", xlog = logscale , xmin = 1.8, xmax = 2200., caption="Coupled Dark Energy $Q = \frac{\partial \ln m_{DM}}{\partial \phi}$")
+  endif
   do l=lmin, lmax
      ells(l) = l
   enddo
@@ -58,20 +69,62 @@ program test
   call fod%set_standard_cosmology(Omega_b=0.0485374d0, Omega_c=0.2585497252d0, h = 0.67766d0, tau_re = 0.08193d0, As = 2.2098d-9, ns = 0.968d0, nrun = 0.d0, r = 0.d0, nt = -0.01d0, YHe = 0.248d0, Nnu = 3.d0, de_Q = 0.d0, de_tracking_n = tracking_n)
   call fod%compute_source(0)
   call fod%source(0)%get_All_Cls(lmin, lmax, Cls_scalar)
-  call fig%curve(ells, Cls_scalar(coop_index_ClTT,:)*ells*(ells+1.d0)*(norm/coop_2pi), linewidth=1., color = "black", linetype="solid", legend="$Q = 0$")
+  Cls_scalar_Q0 = Cls_scalar  
+  if(plot_lensed)then
+     call coop_get_lensing_Cls(lmin, lmax, Cls_Scalar, Cls_lensed)
+     Cls_lensed = Cls_lensed+Cls_scalar
+     Cls_lensed_Q0 = Cls_lensed
+     if(plot_diff)then
+        Cls_lensed = 0.d0
+     endif
+     call fig%curve(ells, Cls_lensed(coop_index_ClTT,:)*ells*(ells+1.d0)*(norm/coop_2pi), linewidth=1., color = "black", linetype="solid", legend="$Q = 0$")
+  else
+     if(plot_diff)then
+        Cls_scalar = 0.d0
+     endif     
+     call fig%curve(ells, Cls_scalar(coop_index_ClTT,:)*ells*(ells+1.d0)*(norm/coop_2pi), linewidth=1., color = "black", linetype="solid", legend="$Q = 0$")
+  endif
   
   call fod%set_standard_cosmology(Omega_b=0.0485374d0, Omega_c=0.2585497252d0, h = 0.67766d0, tau_re = 0.08193d0, As = 2.2098d-9, ns = 0.968d0, nrun = 0.d0, r = 0.d0, nt = -0.01d0, YHe = 0.248d0, Nnu = 3.d0, de_Q = 0.1d0, de_tracking_n = tracking_n)
   call fod%compute_source(0)
   call fod%source(0)%get_All_Cls(lmin, lmax, Cls_scalar)
-  call fig%curve(ells, Cls_scalar(coop_index_ClTT,:)*ells*(ells+1.d0)*(norm/coop_2pi), linewidth=1., color="blue", linetype="dashed", legend="$Q = 0.1$")
+  if(plot_lensed)then
+     call coop_get_lensing_Cls(lmin, lmax, Cls_Scalar, Cls_lensed)
+     Cls_lensed = Cls_lensed+Cls_scalar
+     if(plot_diff)then
+        Cls_lensed = Cls_lensed - Cls_lensed_Q0
+     endif     
+     call fig%curve(ells, Cls_lensed(coop_index_ClTT,:)*ells*(ells+1.d0)*(norm/coop_2pi), linewidth=1., color="blue", linetype="dashed", legend="$Q = 0.1$")
+  else
+     if(plot_diff)then
+        Cls_scalar = Cls_scalar - Cls_scalar_Q0
+     endif          
+     call fig%curve(ells, Cls_scalar(coop_index_ClTT,:)*ells*(ells+1.d0)*(norm/coop_2pi), linewidth=1., color="blue", linetype="dashed", legend="$Q = 0.1$")
+  endif
 
   call fod%set_standard_cosmology(Omega_b=0.0485374d0, Omega_c=0.2585497252d0, h = 0.67766d0, tau_re = 0.08193d0, As = 2.2098d-9, ns = 0.968d0, nrun = 0.d0, r = 0.d0, nt = -0.01d0, YHe = 0.248d0, Nnu = 3.d0, de_Q = 0.2d0, de_tracking_n = tracking_n)
   call fod%compute_source(0)
   call fod%source(0)%get_All_Cls(lmin, lmax, Cls_scalar)
-  call fig%curve(ells, Cls_scalar(coop_index_ClTT,:)*ells*(ells+1.d0)*(norm/coop_2pi), linewidth=1.5,  color="red", linetype="dotted", legend="$Q = 0.2$")
+  if(plot_lensed)then
+     call coop_get_lensing_Cls(lmin, lmax, Cls_Scalar, Cls_lensed)
+     Cls_lensed = Cls_lensed+Cls_scalar
+     if(plot_diff)then
+        Cls_lensed = Cls_lensed - Cls_lensed_Q0
+     endif          
+     call fig%curve(ells, Cls_lensed(coop_index_ClTT,:)*ells*(ells+1.d0)*(norm/coop_2pi), linewidth=1.5,  color="red", linetype="dotted", legend="$Q = 0.2$")
+  else
+     if(plot_diff)then
+        Cls_scalar = Cls_scalar - Cls_scalar_Q0
+     endif          
+     call fig%curve(ells, Cls_scalar(coop_index_ClTT,:)*ells*(ells+1.d0)*(norm/coop_2pi), linewidth=1.5,  color="red", linetype="dotted", legend="$Q = 0.2$")
+  end if
 
-  call fig%label("$V=V_0\phi^{-0.3}$", 0.1, 0.4, alignment = "left")
-  call fig%legend(0.1, 0.9, 1, .false.)
+  call fig%label("$V=V_0\phi^{-0.3}$", 0.1, 0.4, alignment = "right")
+  if(plot_diff)then
+     call fig%legend(0.3, 0.4, 1, .false.)     
+  else
+     call fig%legend(0.1, 0.9, 1, .false.)
+  endif
   call fig%close()
   
 end program test
