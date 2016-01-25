a162401ee91d05c245935beccf782839470a6a92
zqhuang <zhiqihuang@gmail.com>
2016-01-25 13:42:56 -0500

    update fstack

--- mapio/flat_stack.f90
+++ mapio/flat_stack.f90
@@ -10,16 +10,20 @@ program test
   COOP_INT,parameter::lmax = 2500
   COOP_INT,parameter::irepeat = 1
   COOP_INT,parameter::fwhm_arcmin = 5
-  COOP_REAL, parameter::reg_limit = 0.01
-  COOP_UNKNOWN_STRING,parameter::mapdir = "act16/"
-  COOP_UNKNOWN_STRING, parameter::postfix="7ar2"
+  COOP_REAL, parameter::reg_limit = 0.02
+  COOP_UNKNOWN_STRING,parameter::mapdir = "act15/"
+  COOP_UNKNOWN_STRING, parameter::postfix="4"
   COOP_UNKNOWN_STRING,parameter::Ifile = mapdir//"dataCoadd_I_"//postfix//".fits"
   COOP_UNKNOWN_STRING,parameter::Qfile = mapdir//"dataCoadd_Q_"//postfix//".fits"
   COOP_UNKNOWN_STRING,parameter::Ufile = mapdir//"dataCoadd_U_"//postfix//".fits"
-  COOP_UNKNOWN_STRING,parameter::Hitsfile = mapdir//"mask_"//postfix//".fits"
+  COOP_UNKNOWN_STRING,parameter::I_Hitsfile = mapdir//"weightMap_"//postfix//".fits"
+  COOP_UNKNOWN_STRING,parameter::Q_Hitsfile = mapdir//"weightMap_"//postfix//"q.fits"
+  COOP_UNKNOWN_STRING,parameter::U_Hitsfile = mapdir//"weightMap_"//postfix//"u.fits"
+  !!  COOP_UNKNOWN_STRING,parameter::Hitsfile = mapdir//"NULLFILE.fits"
+  !!  COOP_UNKNOWN_STRING,parameter::PSfile = mapdir//"NULLFILE.fits"
   COOP_UNKNOWN_STRING,parameter::PSfile = mapdir//"joinedClusterMasks_"//postfix//".fits"
-!!  COOP_UNKNOWN_STRING,parameter::PSfile = mapdir//"rahulMasksS2_"//postfix//"_cutlevel20.fits"    
-  type(coop_fits_image_cea)::imap, umap, qmap, hits, psmask
+  !!  COOP_UNKNOWN_STRING,parameter::PSfile = mapdir//"rahulMasksS2_"//postfix//"_cutlevel20.fits"    
+  type(coop_fits_image_cea)::imap, umap, qmap, I_hits,Q_hits, U_hits, psmask
   type(coop_asy)::asy
   COOP_INT i, l
   type(coop_file) fp
@@ -28,91 +32,150 @@ program test
   COOP_REAL::mask_threshold
   COOP_REAL, parameter::smooth_scale = coop_SI_arcmin * fwhm_arcmin
   type(coop_healpix_maps)::hp, mask
+  logical:: has_mask = .false.
+  logical::has_hits = .false.
   call coop_MPI_Init()
-  call imap%open(Ifile)
-  call qmap%open(Qfile)
-  call umap%open(Ufile)
-  if(coop_file_exists(hitsFile))then
-     call hits%open(Hitsfile)
-  else
-     write(*,*) "Hits file "//trim(hitsfile)//" is not found; skipping..."     
-     hits = imap
-     hits%image = 1.
-  endif
+  call hp%init(nside=2048, nmaps=3, genre="IQU", lmax=lmax)
+  call mask%init(nside=2048, nmaps=1, genre="MASK", lmax=lmax)  
+
   if(coop_file_exists(PSfile))then
      call psmask%open(PSFile)
+     has_mask = .true.
+     imap%image = imap%image
   else
      write(*,*) "Cluster mask file "//trim(psfile)//" is not found; skipping..."
-     psmask = imap
-     psmask%image = 1.
+     has_mask = .false.
   endif
-  print*,"# of pixels (I, Q, U, mask, ps):", imap%npix, qmap%npix, umap%npix, hits%npix, psmask%npix
-  
-  write(*,*) "max values:", maxval(abs(imap%image)), maxval(abs(qmap%image)), maxval(abs(umap%image))
-  print*,"============comparing <T> in sources and global <T> ====="
-  print*, "<T> in sources:", sum(imap%image*(1.-psmask%image)*hits%image)/sum((1.-psmask%image)*hits%image)
-  print*, "global <T>:", sum(imap%image*hits%image)/sum(hits%image)  
-  print*,"============masking sources===================="  
-  hits%image = hits%image * psmask%image
-  imap%image = imap%image*psmask%image
-  qmap%image = qmap%image*psmask%image
-  umap%image = umap%image*psmask%image
-  write(*,*) "max values:", maxval(abs(imap%image)), maxval(abs(qmap%image)), maxval(abs(umap%image))  
-  print*,"============regularizing===================="
+  !!imap 
+  call imap%open(Ifile)
+  if(has_mask)imap%image = imap%image*psmask%image
+  if(coop_file_exists(I_hitsFile))then
+     call I_hits%open(I_Hitsfile)
+     where(I_hits%image .lt. 10. )
+        imap%image = 0.
+        I_hits%image = 0.
+     end where
+     if(has_mask) I_hits%image = I_hits%image*psmask%image
+     has_hits = .true.
+  else
+     write(*,*) "Hits file "//trim(I_hitsfile)//" is not found; skipping..." 
+     has_hits = .false.
+  endif
+  write(*,*) "Before regularization, I map min, max:", minval(imap%image), maxval(imap%image)
   call imap%regularize(reg_limit)
-  call qmap%regularize(reg_limit)
-  call umap%regularize(reg_limit)
-  write(*,*) "max values:", maxval(abs(imap%image)), maxval(abs(qmap%image)), maxval(abs(umap%image))  
-  print*,"=================================="  
-  if(do_convert)then
-     call hp%init(nside=2048, nmaps=3, genre="IQU", lmax=lmax)
-     call mask%init(nside=2048, nmaps=1, genre="MASK", lmax=lmax)  
-     call imap%convert2healpix(hp, 1, mask, hits=hits)
-     call qmap%convert2healpix(hp, 2, mask, hits=hits)
-     call umap%convert2healpix(hp, 3, mask, hits=hits)
-     call hp%smooth(fwhm = smooth_scale, l_lower =lmin, l_upper = lmax)
-     print*,"===== smoothed map min max ====="  
-     print*, maxval(hp%map(:,1)), minval(hp%map(:,1))
-     print*, maxval(hp%map(:,2)), minval(hp%map(:,2))
-     print*, maxval(hp%map(:,3)), minval(hp%map(:,3))
-     print*, "====  fsky = "//trim(coop_num2str(count(mask%map(:,1).gt.0.5)/dble(mask%npix)*100., "(F10.2)"))//"%======="
-     
-     print*,"=================================="  
-
-     call hp%write(mapdir//"act_iqu_"//COOP_STR_OF(fwhm_arcmin)//"a_l"//COOP_STR_OF(lmin)//"-"//COOP_STR_OF(lmax)//".fits")
-     call hp%write(mapdir//"act_qu_"//COOP_STR_OF(fwhm_arcmin)//"a_l"//COOP_STR_OF(lmin)//"-"//COOP_STR_OF(lmax)//".fits", index_list=(/ 2, 3/) )     
-     call hp%get_QU()
-     call hp%write(mapdir//"act_TQTUT_"//COOP_STR_OF(fwhm_arcmin)//"a_l"//COOP_STR_OF(lmin)//"-"//COOP_STR_OF(lmax)//".fits")
-     call mask%write(mapdir//"act_mask.fits")
-     stop
+  write(*,*) "After regularization, I map min, max:", minval(imap%image), maxval(imap%image)
+  if(has_hits)then
+     call imap%convert2healpix(hp, 1, mask, hits=I_hits)
+     call I_hits%free()
+  else
+     if(has_mask)then
+        call imap%convert2healpix(hp, 1, mask, hits=psmask)
+     else
+        call imap%convert2healpix(hp, 1, mask)
+     endif
   endif
-  mask_threshold = maxval(hits%image)*0.2
-  where (hits%image .lt. mask_threshold)
-     hits%image = 0.
-  elsewhere
-     hits%image = 1.
-  end where
-  call hits%get_flatmap(smooth_scale)
-  imap%image = imap%image*hits%image
-  qmap%image = qmap%image*hits%image
-  umap%image = umap%image*hits%image    
-  call imap%get_flatmap(smooth_scale)
-  call qmap%get_flatmap(smooth_scale)
-  call umap%get_flatmap(smooth_scale)
-  call imap%get_QU(qmap, umap)
-  call imap%smooth_flat(lmin = lmin, lmax = lmax)
-  call qmap%smooth_flat(lmin = lmin, lmax = lmax)
-  call umap%smooth_flat(lmin = lmin, lmax = lmax)
-  call coop_random_init()
-  call imap%find_extrema(hits, "spots/act_Tmax.txt", "Tmax", patchsize, irepeat)
-  call imap%stack2fig("spots/act_Tmax.txt", "T", patchsize, output_dir//"act_T_onTmax.txt", caption="$T$ on $T_{\max}$", label = "$T (\mu K)$", color_table = "Rainbow")
-!$  call system("../utils/fasy.sh "//output_dir//"act_T_onTmax.txt")  
-  call imap%stack2fig("spots/act_Tmax.txt", "Qr", patchsize, output_dir//"act_Qr_onTmax.txt", caption="$Q_r$ on $T_{\max}$", label = "$Q_r (\mu K)$", color_table = "Rainbow")
-!$  call system("../utils/fasy.sh "//output_dir//"act_Qr_onTmax.txt")
+  call imap%free()
+  print*, "I map done"
+  print*, "==== I fsky = "//trim(coop_num2str(count(mask%map(:,1).gt.0.5)/dble(mask%npix)*100., "(F10.2)"))//"%======="
+  call mask%write(mapdir//"act_imask.fits")
 
-  call imap%stack2fig("spots/act_Tmax.txt", "Q", patchsize, output_dir//"act_Q_onTmax.txt", caption="$Q$ on $T_{\max}$", label = "$Q (\mu K)$", color_table = "Rainbow")
-!$  call system("../utils/fasy.sh "//output_dir//"act_Q_onTmax.txt")
-  
-  call coop_MPI_FInalize()
+  !!qmap
+  call qmap%open(Qfile)
+  if(has_mask)qmap%image = qmap%image*psmask%image
+  if(coop_file_exists(Q_hitsFile))then
+     call Q_hits%open(Q_Hitsfile)
+     where(Q_hits%image .lt. 10.)
+        qmap%image = 0.
+        Q_hits%image = 0.
+     end where
+     if(has_mask)Q_hits%image = Q_hits%image * psmask%image
+     has_hits = .true.
+  else
+     write(*,*) "Hits file "//trim(Q_hitsfile)//" is not found; skipping..."    
+     has_hits = .false.
+  endif
+  write(*,*) "Before regularization, Q map min, max:", minval(qmap%image), maxval(qmap%image)
+  call qmap%regularize(reg_limit)
+  write(*,*) "After regularization, Q map min, max:", minval(qmap%image), maxval(qmap%image)
+  if(has_hits)then
+     call qmap%convert2healpix(hp, 2, mask, hits=Q_hits)
+     call Q_hits%free()
+  else
+     if(has_mask)then
+        call qmap%convert2healpix(hp, 2, mask, hits=psmask)
+     else
+        call qmap%convert2healpix(hp, 2, mask)
+     endif
+  endif
+  call qmap%free()
+  print*, "==== Q fsky = "//trim(coop_num2str(count(mask%map(:,1).gt.0.5)/dble(mask%npix)*100., "(F10.2)"))//"%======="
 
-end program test
+  call mask%write(mapdir//"act_qmask.fits")
+
+  !! u map
+  call umap%open(Ufile)
+  if(coop_file_exists(U_hitsFile))then
+     call U_hits%open(U_Hitsfile)
+     where(U_hits%image .lt. 10.)
+        umap%image = 0.
+        U_hits%image = 0.
+     end where
+     if(has_mask)U_hits%image = U_hits%image * psmask%image
+     has_hits = .true.
+  else
+     write(*,*) "Hits file "//trim(U_hitsfile)//" is not found; skipping..."
+     has_hits = .false.
+  endif
+  write(*,*) "Before regularization, U map min, max:", minval(umap%image), maxval(umap%image)
+  call umap%regularize(reg_limit)
+  write(*,*) "After regularization, U map min, max:", minval(umap%image), maxval(umap%image)
+
+  if(has_hits)then
+     call umap%convert2healpix(hp, 3, mask, hits=U_hits)
+     call U_hits%free()
+  else
+     if(has_mask)then
+        call umap%convert2healpix(hp, 3, mask, hits=psmask)
+        call psmask%free()
+     else
+        call umap%convert2healpix(hp, 3, mask)
+     endif
+  endif
+  call umap%free()
+  print*, "==== U fsky = "//trim(coop_num2str(count(mask%map(:,1).gt.0.5)/dble(mask%npix)*100., "(F10.2)"))//"%======="
+  call mask%write(mapdir//"act_umask.fits")
+  call mask%free()
+  call hp%smooth(fwhm = smooth_scale, l_lower =lmin, l_upper = lmax)
+  print*,"===== smoothed map max min ====="  
+  print*, maxval(hp%map(:,1)), minval(hp%map(:,1))
+  print*, maxval(hp%map(:,2)), minval(hp%map(:,2))
+  print*, maxval(hp%map(:,3)), minval(hp%map(:,3))
+  print*,"=================================="  
+
+  call hp%write(mapdir//"act_iqu_"//COOP_STR_OF(fwhm_arcmin)//"a_l"//COOP_STR_OF(lmin)//"-"//COOP_STR_OF(lmax)//".fits")
+  call hp%write(mapdir//"act_qu_"//COOP_STR_OF(fwhm_arcmin)//"a_l"//COOP_STR_OF(lmin)//"-"//COOP_STR_OF(lmax)//".fits", index_list=(/ 2, 3/) )     
+  call hp%get_QU()
+  call hp%write(mapdir//"act_TQTUT_"//COOP_STR_OF(fwhm_arcmin)//"a_l"//COOP_STR_OF(lmin)//"-"//COOP_STR_OF(lmax)//".fits")
+  call hp%free()
+!!$  else
+!!$     call I_hits%get_flatmap(smooth_scale)
+!!$     call Q_hits%get_flatmap(smooth_scale)
+!!$     call U_hits%get_flatmap(smooth_scale)
+!!$     imap%image = imap%image*I_hits%image
+!!$     qmap%image = qmap%image*Q_hits%image
+!!$     umap%image = umap%image*U_hits%image    
+!!$     call imap%get_flatmap(smooth_scale)
+!!$     call qmap%get_flatmap(smooth_scale)
+!!$     call umap%get_flatmap(smooth_scale)
+!!$     call imap%get_QU(qmap, umap)
+!!$     call imap%smooth_flat(lmin = lmin, lmax = lmax)
+!!$     call qmap%smooth_flat(lmin = lmin, lmax = lmax)
+!!$     call umap%smooth_flat(lmin = lmin, lmax = lmax)
+!!$     call coop_random_init()
+!!$     call imap%find_extrema(I_hits, "spots/act_Tmax.txt", "Tmax", patchsize, irepeat)
+!!$     call imap%stack2fig("spots/act_Tmax.txt", "T", patchsize, output_dir//"act_T_onTmax.txt", caption="$T$ on $T_{\max}$", label = "$T (\mu K)$", color_table = "Rainbow")
+!!$     call imap%stack2fig("spots/act_Tmax.txt", "Qr", patchsize, output_dir//"act_Qr_onTmax.txt", caption="$Q_r$ on $T_{\max}$", label = "$Q_r (\mu K)$", color_table = "Rainbow")
+!!$     call imap%stack2fig("spots/act_Tmax.txt", "Q", patchsize, output_dir//"act_Q_onTmax.txt", caption="$Q$ on $T_{\max}$", label = "$Q (\mu K)$", color_table = "Rainbow")
+!!$  endif
+
+   end program test
--- mapio/simulate_maps.f90
+++ mapio/simulate_maps.f90
@@ -10,58 +10,59 @@ program test
 
   implicit none
 #include "constants.h"
-  COOP_INT,parameter::lmax = 100
-  COOP_INT,parameter::nside = 256
-  COOP_INT,parameter::nside_output = 16  
-  COOP_REAL,parameter::beam_fwhm = 40.
-  logical,parameter::do_constrained = .false.
+  COOP_INT,parameter::lmax = 2500
+  COOP_INT,parameter::lmin = 250
+  COOP_INT,parameter::nside = 2048
+!  COOP_INT,parameter::nside_output = 16  
+  COOP_REAL,parameter::beam_fwhm = 5.
+  COOP_INT,parameter::hp_lowl = lmin-10
+  COOP_INT,parameter::hp_highl = lmax+10
+!!  logical,parameter::do_constrained = .false.
   type(coop_healpix_maps)::map,  lmap, mask, hmap
   integer l, m, il, i
   type(coop_file)::fp
-  COOP_UNKNOWN_STRING, parameter::prefix = "mocklowl/sim"
-  COOP_REAL:: sigma, Cls(0:lmax)
-  type(coop_healpix_inpaint)::inp
+  COOP_UNKNOWN_STRING, parameter::prefix = "act16/sim"
+  COOP_REAL:: Cls(0:lmax)
+!  type(coop_healpix_inpaint)::inp
+  COOP_REAL::sigmasq
   call coop_MPI_Init()  
   call coop_random_init()
   !!load Cls
   Cls = 0.d0
-  sigma = coop_sigma_by_fwhm * beam_fwhm * coop_SI_arcmin  
+  sigmasq = 0.
   call fp%open("planck14best_lensedCls.dat", "r")
   do l=2, lmax
      read(fp%unit, *) il, Cls(l)
      if(il.ne. l) stop "Cl file error"     
-     Cls(l) = Cls(l)*(coop_2pi/l/(l+1.d0))*coop_gaussian_filter(beam_fwhm, l)**2
+     Cls(l) = Cls(l)*(coop_2pi/l/(l+1.d0))*coop_gaussian_filter(beam_fwhm, l)**2*coop_highpass_filter(hp_lowl, hp_highl, l)**2
+     sigmasq = sigmasq + (2.*l+1.)*Cls(l)
   enddo
+  sigmasq = sigmasq/coop_4pi
   call fp%close()
-  if(do_constrained)then
-     call map%read("lowl/commander_dx11d2_extdata_temp_cmb_n0256_60arc_v1_cr.fits")
-     call lmap%init(nside = nside_output, nmaps = 1, genre = "TEMPERATURE", nested = .true.)
-     call hmap%init(nside = 64, nmaps = 1, genre = "TEMPERATURE", nested = .true.)          
-     call mask%read("lowl/mask_hot_bar_n0256.fits")
-     call map%apply_mask(mask)
-     call inp%init(map = map, mask = mask, lmax = lmax, cls = cls)
-     do i=1, 1000
-        write(*,*) "simulating map #"//COOP_STR_OF(i)
-        call inp%upgrade(reset = .true.)
-        call inp%upgrade()
-        call inp%upgrade()
-        call inp%upgrade()
-        hmap%map = inp%lMT%map + inp%lCT%map
-        call hmap%smooth(coop_SI_arcmin*beam_fwhm)
-        call coop_healpix_maps_ave_udgrade(hmap, lmap)
-        call lmap%write(trim(prefix)//"_inp_"//COOP_STR_OF(lmap%nside)//"_"//COOP_STR_OF(nint(beam_fwhm))//"a_"//COOP_STR_OF(i)//".fits")        
-     enddo
-  else
-     !! Cls(2) =  254.90171756233437     
-     call map%init(nside = nside, nmaps=1, genre="TEMPERATURE", lmax = lmax)
-     call lmap%init(nside = nside_output, nmaps = 1, genre = "TEMPERATURE")
-     do i=1, 100
-        map%Cl(:,1) = Cls        
-        write(*,*) "simulating map #"//COOP_STR_OF(i)        
-        call map%simulate()
-!        call coop_healpix_maps_ave_udgrade(map, lmap)
-        call map%write(trim(prefix)//"_full_"//COOP_STR_OF(map%nside)//"_"//COOP_STR_OF(nint(beam_fwhm))//"a_"//COOP_STR_OF(i)//".fits")
-     enddo
-  endif
-  call coop_MPI_Finalize()
+  call map%init(nside = nside, nmaps=1, genre="TEMPERATURE", lmax = lmax)
+  map%Cl = 0.
+  map%Cl(2:lmax,1) = Cls(2:lmax)        
+  call map%simulate()
+  call map%write(trim(prefix)//"_n"//COOP_STR_OF(map%nside)//"_"//COOP_STR_OF(nint(beam_fwhm))//"a_l"//COOP_STR_OF(lmin)//"-"//COOP_STR_OF(lmax)//".fits")
+  write(*,*) "map rms expected to be about: ", sqrt(sigmasq)
+  write(*,*) "actual map rms: ", sqrt(sum(map%map**2)/map%npix)
+!!$  if(do_constrained)then
+!!$     call map%read("lowl/commander_dx11d2_extdata_temp_cmb_n0256_60arc_v1_cr.fits")
+!!$     call lmap%init(nside = nside_output, nmaps = 1, genre = "TEMPERATURE", nested = .true.)
+!!$     call hmap%init(nside = 64, nmaps = 1, genre = "TEMPERATURE", nested = .true.)          
+!!$     call mask%read("lowl/mask_hot_bar_n0256.fits")
+!!$     call map%apply_mask(mask)
+!!$     call inp%init(map = map, mask = mask, lmax = lmax, cls = cls)
+!!$     do i=1, 1000
+!!$        write(*,*) "simulating map #"//COOP_STR_OF(i)
+!!$        call inp%upgrade(reset = .true.)
+!!$        call inp%upgrade()
+!!$        call inp%upgrade()
+!!$        call inp%upgrade()
+!!$        hmap%map = inp%lMT%map + inp%lCT%map
+!!$        call hmap%smooth(coop_SI_arcmin*beam_fwhm)
+!!$        call coop_healpix_maps_ave_udgrade(hmap, lmap)
+!!$        call lmap%write(trim(prefix)//"_inp_"//COOP_STR_OF(lmap%nside)//"_"//COOP_STR_OF(nint(beam_fwhm))//"a_"//COOP_STR_OF(i)//".fits")        
+!!$     enddo
+
 end program test
