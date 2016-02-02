94540bf184cd006f796675cd326602888519242c
zqhuang <zhiqihuang@gmail.com>
2016-02-02 14:21:12 -0500

    update fsim

--- mapio/flat_sim.f90
+++ mapio/flat_sim.f90
@@ -10,11 +10,13 @@ program test
   COOP_INT,parameter::lmin =   200
   COOP_INT,parameter::lmax = 4000
   COOP_REAL,parameter::reg_limit = 0.001d0
+  COOP_INT::isim
   type(coop_fits_image_cea)::tmap, emap, bmap, noise, qmap, umap
   COOP_REAL::fwhm_arcmin = 0.5
   COOP_REAL::Cls(lmin:lmax,6),junk, bl(lmin:lmax), norm
   COOP_INT::l, il
   type(coop_file)::fp
+  call coop_get_command_line_argument(key = "isim", arg = isim, default = 1)
   call coop_random_init()
   call fp%open_skip_comments(beam_file)
   do l = 0, lmin-1
@@ -43,25 +45,25 @@ program test
   qmap = tmap
   umap = tmap
   call coop_fits_image_cea_simulate_TEB(lmin=lmin, lmax=lmax, Cls=Cls, tmap = tmap, emap = emap, bmap = bmap, qmap=qmap, umap =umap)
-  call tmap%write(mapdir//"sim_2_I.fits")
-  call qmap%write(mapdir//"sim_2_Q.fits")
-  call umap%write(mapdir//"sim_2_U.fits")
+  call tmap%write(mapdir//"sim_"//COOP_STR_OF(isim)//"_I.fits")
+  call qmap%write(mapdir//"sim_"//COOP_STR_OF(isim)//"_Q.fits")
+  call umap%write(mapdir//"sim_"//COOP_STR_OF(isim)//"_U.fits")
 
   call emap%free()
   call bmap%free()
-  call noise%open(mapdir//"deep56_array_2_noise_sim_2_I.fits")
+  call noise%open(mapdir//"deep56_array_2_noise_sim_"//COOP_STR_OF(isim)//"_I.fits")
   call noise%regularize(reg_limit)
   tmap%image = tmap%image + noise%image
-  call tmap%write(mapdir//"sim_with_noise_2_I.fits")
+  call tmap%write(mapdir//"sim_with_noise_"//COOP_STR_OF(isim)//"_I.fits")
 
-  call noise%open(mapdir//"deep56_array_2_noise_sim_2_Q.fits")
+  call noise%open(mapdir//"deep56_array_2_noise_sim_"//COOP_STR_OF(isim)//"_Q.fits")
   call noise%regularize(reg_limit)
   qmap%image = qmap%image + noise%image
-  call qmap%write(mapdir//"sim_with_noise_2_Q.fits")
+  call qmap%write(mapdir//"sim_with_noise_"//COOP_STR_OF(isim)//"_Q.fits")
 
-  call noise%open(mapdir//"deep56_array_2_noise_sim_2_U.fits")
+  call noise%open(mapdir//"deep56_array_2_noise_sim_"//COOP_STR_OF(isim)//"_U.fits")
   call noise%regularize(reg_limit)
   umap%image = umap%image + noise%image
-  call umap%write(mapdir//"sim_with_noise_2_U.fits")
+  call umap%write(mapdir//"sim_with_noise_"//COOP_STR_OF(isim)//"_U.fits")
 
 end program test
