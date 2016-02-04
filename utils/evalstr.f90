4357fce277183a43cc96eab8a2b7722db2e4ed42
Zhiqi Huang <zhiqihuang@gmail.com>
2016-02-04 15:20:25 -0500

    add evalstr module

--- utils/evalstr.f90
+++ utils/evalstr.f90
@@ -13,6 +13,9 @@ module coop_evalstr_mod
   COOP_INT, parameter::coop_math_expression_error_unknown_function = 2
   COOP_INT, parameter::coop_math_expression_error_operators_together = 3
   COOP_INT, parameter::coop_math_expression_error_numbers_together = 4
+  COOP_INT, parameter::coop_math_expression_error_extra_dot = 5
+  COOP_INT, parameter::coop_math_expression_error_too_many_numbers = 6
+  COOP_INT, parameter::coop_math_expression_error_invalid_characters = 7
   
   type coop_math_expression 
      COOP_LONG_STRING::expr     !!contains ()+-*/^a-z and digits
@@ -33,158 +36,14 @@ module coop_evalstr_mod
 
 contains
 
-  !!errors : 
-  !! 1 : brackets do not match
-  recursive subroutine coop_math_expression_simplify(this, str, length, err) 
+  subroutine coop_math_expression_init(this, mathexpr, error)
     class(coop_math_expression)::this
-    character(len=:),allocatable::tmp
-    COOP_UNKNOWN_STRING::str
-    COOP_INT::length !!length of the str
-    COOP_INT err
-    COOP_INT::ipos, i, j, nleft, nright, l
-    l = length
-    !!simplify all brackets
-    ipos = scan(str(1:l), "(")
-    do while(ipos .ne. 0)
-       nleft = 1
-       nright = 0
-       i = ipos + 1
-       do
-          j = scan(str(i:l),"()")
-          if(j.eq.0)then
-             err = coop_math_expression_error_brackets_not_match
-             return
-          endif
-          j = i + j -1
-          if(str(j:j).eq."(")then
-             nleft  = nleft + 1
-          else
-             nright = nright + 1
-          endif
-          if(nright .eq. nleft)then
-             allocate(character(len=j-ipos-1)::tmp)
-             tmp = str(ipos+1:j-1)
-             call coop_math_expression_simplify(this, tmp, j-ipos-1, err)
-             if(err.ne.0)return
-             this%n = this%n + 1
-             read(tmp, *) this%vars(this%n)
-             if(ipos.ge.4)then  !!check functions
-                select case(str(ipos-3:ipos-1))
-                case("exp")
-                   this%vars(this%n) = exp(this%vars(this%n))
-                   ipos = ipos - 3
-                case("log")
-                   this%vars(this%n) = log(this%vars(this%n))
-                   ipos = ipos - 3
-                case("sin")
-                   if(ipos .ge. 5)then
-                      if(str(ipos-4:ipos-4).eq."a")then
-                         this%vars(this%n) = asin(this%vars(this%n))                         
-                         ipos = ipos - 4
-                      else
-                         select case(ichar(str(ipos-4:ipos-4))
-                         case(coop_ascii_lower_a : coop_ascii_lower_z)
-                            error = coop_math_expression_error_unknown_function 
-                            return
-                         case(coop_ascii_0:coop_ascii_9, coop_ascii_dot)
-                            error = coop_math_expression_error_numbers_together
-                            return
-                         end select
-                         this%vars(this%n) = sin(this%vars(this%n))
-                         ipos = ipos - 3
-                      endif
-                   else
-                      this%vars(this%n) = sin(this%vars(this%n))
-                      ipos = ipos - 3
-                   endif
-                case("cos")
-                   if(ipos.ge.5)then
-                      if(str(ipos-4:ipos-4).eq."a")then
-                         this%vars(this%n) = acos(this%vars(this%n))                         
-                         ipos = ipos - 4
-                      else
-                         select case(ichar(str(ipos-4:ipos-4))
-                         case(coop_ascii_lower_a : coop_ascii_lower_z)
-                            error = coop_math_expression_error_unknown_function 
-                            return
-                         case(coop_ascii_0:coop_ascii_9, coop_ascii_dot)
-                            error = coop_math_expression_error_numbers_together
-                            return
-                         end select
-                         this%vars(this%n) = cos(this%vars(this%n))
-                         ipos = ipos - 3
-                      endif
-                   else
-                      this%vars(this%n) = cos(this%vars(this%n))
-                      ipos = ipos - 3
-                   endif
-                case("tan")
-                   if(ipos.ge.5)then
-                      if(str(ipos-4:ipos-4).eq."a")then
-                         this%vars(this%n) = atan(this%vars(this%n))                         
-                         ipos = ipos - 4
-                      else
-                         select case(ichar(str(ipos-4:ipos-4))
-                         case(coop_ascii_lower_a : coop_ascii_lower_z)
-                            error = coop_math_expression_error_unknown_function 
-                            return
-                         case(coop_ascii_0:coop_ascii_9, coop_ascii_dot)
-                            error = coop_math_expression_error_numbers_together
-                            return
-                         end select
-                         this%vars(this%n) = tan(this%vars(this%n))
-                         ipos = ipos - 3
-                      endif
-                   else
-                      this%vars(this%n) = tan(this%vars(this%n))
-                      ipos = ipos - 3
-                   endif
-                case("qrt")
-                   if(ipos .ge. 5)then
-                      if(str(ipos-4:ipos-4).eq."s")then
-                         this%vars(this%n) = sqrt(this%vars(this%n))
-                         ipos = ipos - 4
-                      else
-                         error = coop_math_expression_error_unknown_function 
-                         return
-                      endif
-                   else
-                      error = coop_math_expression_error_unknown_function 
-                      return
-                   endif
-                case default
-                   select case(ichar(str(ipos-4:ipos-4))
-                   case(coop_ascii_lower_a : coop_ascii_lower_z)
-                      error = coop_math_expression_error_unknown_function 
-                      return
-                   case(coop_ascii_0:coop_ascii_9, coop_ascii_dot)
-                      error = coop_math_expression_error_numbers_together
-                      return
-                   end select
-                end select
-             endif
-             str = str(1:ipos-1)//"$"//trim(coop_4digits(this%n))//str(j+1:l)
-             l = len_trim(str)
-             exit
-          endif
-          i = j + 1
-          if(i.gt.l)then
-             err = coop_math_expression_error_brackets_not_match
-             return
-          endif
-       enddo
-       ipos = scan(str(1:l), "(")
-    enddo
-    err = 0
-    return
-  end subroutine coop_math_expression_simplify
-
-  subroutine coop_math_expression_init(this, mathexpr)
-    class(coop_math_expression)::this
+    COOP_INT::error
     COOP_UNKNOWN_STRING::mathexpr
     COOP_LONG_STRING::str
     COOP_INT::i, l, j, k
     l = 0
+    error = 0
     do i=1, len_trim(mathexpr)
        select case(ichar(mathexpr(i:i)))
        case(coop_ascii_0:coop_ascii_9, coop_ascii_lower_a:coop_ascii_lower_z, coop_ascii_plus, coop_ascii_dash, coop_ascii_star, coop_ascii_slash, coop_ascii_hat, coop_ascii_dot, coop_ascii_left_bracket, coop_ascii_right_bracket)
@@ -193,6 +52,11 @@ contains
        case(coop_ascii_upper_a:coop_ascii_upper_z)
           l=l+1
           str(l:l) = char(ichar(mathexpr(i:i)) + coop_ascii_lower_minus_upper)
+       case(1:coop_ascii_space)
+          !! do nothing
+       case default
+          error = coop_math_expression_error_invalid_characters
+          return
        end select
     enddo
     str(l+1:) = ""
@@ -236,11 +100,175 @@ contains
           endif
        endif
        this%n = this%n+1
+       if(this%n .gt. coop_math_expression_max_num_vars)then
+          error = coop_math_expression_error_too_many_numbers   
+          return
+       endif
        read(str(i:j-1), *) this%vars(this%n)
        this%expr = trim(this%expr)//"$"//trim(coop_4digits(this%n))
        i = j
     enddo
+    if(scan(this%expr, ".") .ne. 0)then
+       error = coop_math_expression_error_extra_dot
+       return
+    endif
   end subroutine coop_math_expression_init
 
+
+
+  !!errors : 
+  recursive subroutine coop_math_expression_simplify(this, str, length, error) 
+    class(coop_math_expression)::this
+    character(len=:),allocatable::tmp
+    COOP_UNKNOWN_STRING::str
+    COOP_INT::length !!length of the str
+    COOP_INT error
+    COOP_INT::ipos, i, j, nleft, nright, l, ind
+    l = length
+    !!simplify all brackets
+    ipos = scan(str(1:l), "(")
+    do while(ipos .ne. 0)
+       nleft = 1
+       nright = 0
+       i = ipos + 1
+       do
+          j = scan(str(i:l),"()")
+          if(j.eq.0)then
+             error = coop_math_expression_error_brackets_not_match
+             return
+          endif
+          j = i + j -1
+          if(str(j:j).eq."(")then
+             nleft  = nleft + 1
+          else
+             nright = nright + 1
+          endif
+          if(nright .eq. nleft)then
+             allocate(character(len=j-ipos-1)::tmp)
+             tmp = str(ipos+1:j-1)
+             call coop_math_expression_simplify(this, tmp, j-ipos-1, error)
+             if(error.ne.0)return
+             read(tmp(2:5),*) ind
+             if(ipos.ge.4)then  !!check functions
+                select case(str(ipos-3:ipos-1))
+                case("exp")
+                   this%vars(ind) = exp(this%vars(ind))
+                   ipos = ipos - 3
+                case("log")
+                   this%vars(ind) = log(this%vars(ind))
+                   ipos = ipos - 3
+                case("sin")
+                   if(ipos .ge. 5)then
+                      if(str(ipos-4:ipos-4).eq."a")then
+                         this%vars(ind) = asin(this%vars(ind))                         
+                         ipos = ipos - 4
+                      else
+                         select case(ichar(str(ipos-4:ipos-4)))
+                         case(coop_ascii_lower_a : coop_ascii_lower_z)
+                            error = coop_math_expression_error_unknown_function 
+                            return
+                         case(coop_ascii_0:coop_ascii_9)
+                            error = coop_math_expression_error_numbers_together
+                            return
+                         end select
+                         this%vars(ind) = sin(this%vars(ind))
+                         ipos = ipos - 3
+                      endif
+                   else
+                      this%vars(ind) = sin(this%vars(ind))
+                      ipos = ipos - 3
+                   endif
+                case("cos")
+                   if(ipos.ge.5)then
+                      if(str(ipos-4:ipos-4).eq."a")then
+                         this%vars(ind) = acos(this%vars(ind))                         
+                         ipos = ipos - 4
+                      else
+                         select case(ichar(str(ipos-4:ipos-4)))
+                         case(coop_ascii_lower_a : coop_ascii_lower_z)
+                            error = coop_math_expression_error_unknown_function 
+                            return
+                         case(coop_ascii_0:coop_ascii_9)
+                            error = coop_math_expression_error_numbers_together
+                            return
+                         end select
+                         this%vars(ind) = cos(this%vars(ind))
+                         ipos = ipos - 3
+                      endif
+                   else
+                      this%vars(ind) = cos(this%vars(ind))
+                      ipos = ipos - 3
+                   endif
+                case("tan")
+                   if(ipos.ge.5)then
+                      if(str(ipos-4:ipos-4).eq."a")then
+                         this%vars(ind) = atan(this%vars(ind))                         
+                         ipos = ipos - 4
+                      else
+                         select case(ichar(str(ipos-4:ipos-4)))
+                         case(coop_ascii_lower_a : coop_ascii_lower_z)
+                            error = coop_math_expression_error_unknown_function 
+                            return
+                         case(coop_ascii_0:coop_ascii_9)
+                            error = coop_math_expression_error_numbers_together
+                            return
+                         end select
+                         this%vars(ind) = tan(this%vars(ind))
+                         ipos = ipos - 3
+                      endif
+                   else
+                      this%vars(ind) = tan(this%vars(ind))
+                      ipos = ipos - 3
+                   endif
+                case("qrt")
+                   if(ipos .ge. 5)then
+                      if(str(ipos-4:ipos-4).eq."s")then
+                         this%vars(ind) = sqrt(this%vars(ind))
+                         ipos = ipos - 4
+                      else
+                         error = coop_math_expression_error_unknown_function 
+                         return
+                      endif
+                   else
+                      error = coop_math_expression_error_unknown_function 
+                      return
+                   endif
+                case default
+                   select case(ichar(str(ipos-4:ipos-4)))
+                   case(coop_ascii_lower_a : coop_ascii_lower_z)
+                      error = coop_math_expression_error_unknown_function 
+                      return
+                   case(coop_ascii_0:coop_ascii_9)
+                      error = coop_math_expression_error_numbers_together
+                      return
+                   end select
+                end select
+             endif
+             str = str(1:ipos-1)//tmp(1:5)//str(j+1:l)
+             l = l+(ipos+4-j)
+             exit
+          endif
+          i = j + 1
+          if(i.gt.l)then
+             error = coop_math_expression_error_brackets_not_match
+             return
+          endif
+       enddo
+       ipos = scan(str(1:l), "(")
+    enddo
+    !!now search for ^
+    ipos = scan(str(1:l), "^")
+    do while(ipos .ne. 0)
+       if(str(ipos-5:ipos-5) .ne. "$" .or. str(ipos+1:ipos+1).ne."$")then
+          error = coop_math_expression_error_operators_together
+          return
+       endif
+       read(str(ipos-4:ipos-1),*) nleft
+       read(str(ipos+2:ipos+5),*) nleft
+       ipos = scan(str(1:l), "^")
+    enddo
+    error = 0
+    return
+  end subroutine coop_math_expression_simplify
 
 end module coop_evalstr_mod
