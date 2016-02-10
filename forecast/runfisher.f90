f85c5953a84eda9440677e1d2f7952aa2de7a9b9
Zhiqi Huang <zhiqihuang@gmail.com>
2016-02-10 08:02:46 -0500

    CMB fisher working

--- forecast/runfisher.f90
+++ forecast/runfisher.f90
@@ -4,16 +4,27 @@ program RunMC
   implicit none
 #include "constants.h"
   type(coop_fisher)::fisher
+  type(coop_file)::fp
+  COOP_STRING::fname
   COOP_INT::i
   if(iargc() .lt. 1) stop "Fisher input_file"
   call fisher%init(coop_InputArgs(1))
+  fname = trim(fisher%settings%value("output"))
+  if(trim(fname).eq."")then
+     "you need to specify output in "//trim(coop_InputArgs(1))
+     stop
+  endif
+
   call fisher%get_fisher()
+
+  call fp%open(fname)
   do i=1, fisher%n_params_used
-     write(*,"("//COOP_STR_OF(fisher%n_params_used)//"G14.5)") fisher%cov(fisher%ind_used(i), fisher%ind_used) 
+     write(fp%unit,"("//COOP_STR_OF(fisher%n_params_used)//"G14.5)") fisher%cov(fisher%ind_used(i), fisher%ind_used) 
   enddo
   
   do i=1, fisher%n_params_used
-     write(*,*) trim(fisher%paramtable%key(fisher%ind_used(i))), sqrt(fisher%cov(fisher%ind_used(i), fisher%ind_used(i)))
+     write(fp%unit, "(A, G14.5)") trim(fisher%paramtable%key(fisher%ind_used(i))), sqrt(fisher%cov(fisher%ind_used(i), fisher%ind_used(i)))
+     write(*, "(A, G14.5)") trim(fisher%paramtable%key(fisher%ind_used(i))), sqrt(fisher%cov(fisher%ind_used(i), fisher%ind_used(i)))
   enddo
 
 end program RunMC
