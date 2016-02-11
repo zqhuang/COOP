529761d98b5a175ee13a556c371c4e43b4f2473c
Zhiqi Huang <zhiqihuang@gmail.com>
2016-02-10 19:10:12 -0500

    fisher clean up

--- forecast/runfisher.f90
+++ forecast/runfisher.f90
@@ -1,31 +1,42 @@
-program RunMC
+program RunF
   use coop_wrapper_firstorder
   use coop_fisher_mod
   implicit none
 #include "constants.h"
   type(coop_fisher)::fisher
   type(coop_file)::fp
-  COOP_STRING::fname
-  COOP_INT::i
+  COOP_STRING::root
+  COOP_INT::i, j
   if(iargc() .lt. 1) stop "Fisher input_file"
   call fisher%init(coop_InputArgs(1))
-  fname = trim(fisher%settings%value("output"))
-  if(trim(fname).eq."")then
-     write(*,*) "you need to specify output in "//trim(coop_InputArgs(1))
+  root = trim(fisher%settings%value("root"))
+  if(trim(root).eq."")then
+     write(*,*) "you need to specify root in "//trim(coop_InputArgs(1))
      stop
   endif
 
   call fisher%get_fisher()
 
-  call fp%open(fname)
+  call fp%open(trim(root)//"_fisher.txt")
+  write(fp%unit, "(A2, "//COOP_STR_OF(fisher%n_params_used)//"A16)") "# ", fisher%paramtable%key(fisher%ind_used)
   do i=1, fisher%n_params_used
-     write(fp%unit,"("//COOP_STR_OF(fisher%n_params_used)//"G14.5)") fisher%cov(fisher%ind_used(i), fisher%ind_used) 
-  enddo
-  
-  do i=1, fisher%n_params_used
-     write(fp%unit, "(A, G14.5)") trim(fisher%paramtable%key(fisher%ind_used(i))), sqrt(fisher%cov(fisher%ind_used(i), fisher%ind_used(i)))
-     write(*, "(A, G14.5)") trim(fisher%paramtable%key(fisher%ind_used(i))), sqrt(fisher%cov(fisher%ind_used(i), fisher%ind_used(i)))
+     write(fp%unit,"("//COOP_STR_OF(fisher%n_params_used)//"G16.7)") fisher%fisher(fisher%ind_used(i), fisher%ind_used) 
   enddo
   call fp%close()
 
-end program RunMC
+
+  call fp%open(trim(root)//"_cov.txt")
+  write(fp%unit, "(A2, "//COOP_STR_OF(fisher%n_params_used)//"A16)") "# ", fisher%paramtable%key(fisher%ind_used)
+  do i=1, fisher%n_params_used
+     write(fp%unit,"("//COOP_STR_OF(fisher%n_params_used)//"G16.7)") fisher%cov(fisher%ind_used(i), fisher%ind_used) 
+  enddo
+  call fp%close()
+
+  call fp%open(trim(root)//"_std.txt")
+  do i=1, fisher%n_params_used
+     write(fp%unit, "(A, G14.5, A5, G14.5)") trim(fisher%paramtable%key(fisher%ind_used(i)))//" = ", fisher%paramtable%val(fisher%ind_used(i)), " +/- ", sqrt(fisher%cov(fisher%ind_used(i), fisher%ind_used(i)))
+     write(*, "(A, G14.5, A5, G14.5)") trim(fisher%paramtable%key(fisher%ind_used(i)))//" = ", fisher%paramtable%val(fisher%ind_used(i)), " +/- ", sqrt(fisher%cov(fisher%ind_used(i), fisher%ind_used(i)))
+  enddo
+  call fp%close()
+
+end program RunF
