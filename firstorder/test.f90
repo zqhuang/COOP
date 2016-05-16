c92e45f92ca61708056fcc621a0e13a092bcdd9b
Zhiqi Huang <zhiqihuang@gmail.com>
2016-05-16 17:37:18 -0400

    modify params_planck.ini

--- firstorder/test.f90
+++ firstorder/test.f90
@@ -3,29 +3,63 @@ program test
   implicit none
 #include "constants.h"
   type(coop_cosmology_firstorder)::cosmology
-  COOP_REAL::a, b
   type(coop_real_table)::paramtable
   logical success
-  COOP_INT, parameter::nw = 100
-  COOP_REAL,parameter::sigma_W = 5.d0
-  COOP_INT i
-  COOP_REAL::k, kw(nw), wsq(nw), z, pk1, pk2, pk3
+  COOP_REAL::fsky = 0.558
+  COOP_REAL:: z, dz, k
   call paramtable%insert("ombh2", 0.022d0)
-  call paramtable%insert("omch2", 0.11d0)
+  call paramtable%insert("omch2", 0.12d0)
   call paramtable%insert("theta", 1.04d0)
-  call paramtable%insert("tau", 0.07d0)
-  call paramtable%insert("As", 2.d-9)
-  call paramtable%insert("ns", 0.96d0)
-  call coop_prtsystime(.true.)
-  write(*,*) "set up"
+  call paramtable%insert("tau", 0.08d0)
+  call paramtable%insert("As", 2.1d-9)
+  call paramtable%insert("ns", 0.967d0)
   call cosmology%set_up(paramtable, success)
   if(.not. success) stop "initialization failed"
-  print*, cosmology%cosmomc_theta()
-  call coop_prtsystime()
-  write(*,*) "solve pert"
-  call coop_prtsystime(.true.)
   call cosmology%compute_source(0, success)
-  if(.not. success) stop "linear pert failed"
-  call coop_prtsystime()
-  call coop_set_default_zeta_r(cosmology, cosmology%source(0))
+  do 
+     read(*, *) z
+     k = kmax(z)
+     write(*,*) k
+  enddo
+
+contains
+
+  function Volume(z, dz) result(V)
+    COOP_REAL::z, dz, V
+    V = coop_4pi/3.d0*fsky* (cosmology%comoving_angular_diameter_distance(1.d0/(1.d0+z+dz/2.d0))**3 - cosmology%comoving_angular_diameter_distance(1.d0/(1.d0+z-dz/2.d0))**3) * (3000.)**3
+  end function Volume
+
+  function nz(z, dz) 
+    COOP_REAL::z, dz, nz
+    nz = coop_integrate(dndz, z-dz/2.d0, z+dz/2.d0)*(coop_4pi*fsky/coop_SI_arcmin**2) / Volume(z, dz)
+  end function nz
+
+  function dndz(z) 
+    COOP_REAL::z, dndz
+    dndz = 640 *z**2*exp(-z/0.35)
+  end function dndz
+
+  function kmax(z)
+    COOP_REAL::z, kmax, rlow, rhigh, sigmalow, sigmahigh, sigmamid, rmid
+    rlow = 0.5d0
+    rhigh = 20.d0
+    sigmalow = cosmology%sigma_tophat_R(z=z, r = rlow/cosmology%h()*cosmology%H0Mpc())
+    sigmahigh = cosmology%sigma_tophat_R(z=z, r = rhigh/cosmology%h()*cosmology%H0Mpc())
+    if(sigmalow .lt. 0.5d0 .or. sigmahigh .gt. 0.5d0)stop "kmax failed"
+    do while(rhigh/rlow .gt. 1.002)
+       rmid = sqrt(rlow*rhigh)
+       sigmamid = cosmology%sigma_tophat_R(z= z, r  = rmid/cosmology%h()*cosmology%H0Mpc())
+       if(sigmamid .gt. 0.5001d0)then 
+          rlow =rmid
+       elseif(sigmamid .lt. 0.4999d0)then
+          rhigh = rmid
+       else
+          kmax = 1.d0/rmid
+          return
+       endif
+    enddo
+    rmid = sqrt(rlow*rhigh)
+    kmax = coop_pio2/rmid
+  end function kmax
+
 end program test
