690e3edc97295564721e6b42d4c049084e0f5450
Zhiqi Huang <zhiqihuang@gmail.com>
2016-05-22 13:45:40 -0400

    add user-defined models for features

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 mv configure.in configure_local.in
+mv include/user_defined_primordial_power.h include/user_defined_primordial_power_local.h
 git stash save --keep-index
 git pull origin master
 mv configure_local.in configure.in
+mv include/user_defined_primordial_power_local.h include/user_defined_primordial_power.h 
