bed5a58628c6cb1cfb7a067a0d686b6a59612b0c
Zhiqi Huang <zhiqihuang@gmail.com>
2016-06-28 19:55:52 -0400

    update: add sample read

---
+++ nonlinear/read_table_sample.f90
@@ -0,0 +1,26 @@
+program read_table
+  implicit none
+  character(LEN=1024)::file_root, inifile
+  real*8, allocatable::zcol(:,:,:)
+  real*8 fpk_min, fpk_max, e_min, e_max, pbye_min, pbye_max, zmax
+  integer n_fpk, n_e, n_pbye, nz, iz
+  real*8, dimension(:),allocatable::z, t, H, chi, D, H_D
+
+  file_root  = "myoutputs/test"
+  open(10, FILE = trim(adjustl(file_root))//"_zcol.dat", access = 'stream')
+  read(10) n_fpk, fpk_min, fpk_max
+  read(10) n_e, e_min, e_max
+  read(10) n_pbye, pbye_min, pbye_max
+  allocate(zcol(n_fpk, n_e, n_pbye))
+  read(10) zcol
+  close(10)
+  open(10, FILE = trim(adjustl(file_root))//"_background.dat", access = 'stream')
+  read(10) nz, zmax
+  allocate(z(nz), t(nz), H(nz), chi(nz), D(nz), H_D(nz))
+  do iz=1, nz
+     read(10) z(iz), t(iz), H(iz), chi(iz), D(iz), H_D(iz)
+  enddo
+  close(10)
+
+
+end program read_table
