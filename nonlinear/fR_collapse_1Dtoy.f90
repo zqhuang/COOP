fa7053fb018ecea9cff7e67ed28c7d0d0cbde599
Zhiqi Huang <zhiqihuang@gmail.com>
2017-02-21 17:49:29 +0800

    see how to collapse

--- nonlinear/fR_collapse_1Dtoy.f90
+++ nonlinear/fR_collapse_1Dtoy.f90
@@ -3,7 +3,7 @@ program fR1d
   use fR1d_mod
   implicit none
 #include "constants.h"
-  COOP_INT,parameter::nsave = 20
+  COOP_INT,parameter::nsave = 8
   type(coop_fr1d_obj)::halo
   COOP_INT::i, nsteps
   COOP_REAL::delta_ini, z_ini, delta_0, Omega_m, r_halo, zsave(nsave), asave(nsave)
@@ -14,12 +14,13 @@ program fR1d
   delta_0 =  0.6d0*(1.5d0*coop_pi)**(2.d0/3.d0)
   print*, "delta_0 = ", delta_0
   r_halo = 0.001d0
-  zsave = (/ 10.d0, 3.d0, 2.d0, 1.5d0,  1.d0, 0.9d0, 0.8d0, 0.7d0, 0.6d0, 0.5d0, 0.45d0, 0.4d0, 0.35d0, 0.3d0, 0.25d0, 0.2d0, 0.15d0, 0.1d0, 0.05d0, 0.d0/)
+  zsave = (/ 10.d0, 9.d0, 8.d0, 7.d0, 6.d0, 5.52d0, 5.5d0, 5.48d0 /)
+!  zsave = (/ 10.d0, 3.d0, 2.d0, 1.5d0,  1.d0, 0.9d0, 0.8d0, 0.7d0, 0.6d0, 0.5d0, 0.45d0, 0.4d0, 0.35d0, 0.3d0, 0.25d0, 0.2d0, 0.15d0, 0.1d0, 0.05d0, 0.d0/)
   asave = 1.d0/(1.d0+zsave)
   write(*, "(A, E15.4)") "M = ",  0.5d0 * r_halo**3 * coop_SI_PlanckMass * coop_SI_hbyH0/ coop_SI_PlanckTime / coop_SI_Msun / 0.7d0
   delta_ini = delta_0 * coop_Growth_fitting(Omega_m, -1.d0, z_ini) / coop_Growth_fitting(Omega_m, -1.d0, 0.d0)
   delta_ini = delta_ini + 17.d0/21.d0*delta_ini**2 + ((17.d0/21.d0)**2*2.d0-341.d0/567.d0)*delta_ini**3
-  call halo%init(Omega_m = Omega_m, nr = 2048, rmax = r_halo*30.d0, a_ini = 1.d0/(1.d0+z_ini), delta_ini = delta_ini, r_halo = r_halo, bw_halo = r_halo/20.d0, dtau = r_halo/500.d0)
+  call halo%init(Omega_m = Omega_m, nr = 1024, rmax = r_halo*30.d0, a_ini = 1.d0/(1.d0+z_ini), delta_ini = delta_ini, r_halo = r_halo, bw_halo = r_halo/20.d0, dtau = r_halo/500.d0)
   if(trim(coop_InputArgs(1)).eq. "GR")then
      halo%do_GR = .true.
   elseif(trim(coop_InputArgs(1)).eq. "QS")then
